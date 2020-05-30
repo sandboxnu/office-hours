328bdcec6d3e7b8ebc0efa8a920b96b1d61a357f
wstenzel <stenzel.will@gmail.com>
2020-05-30 14:05:30 -0700

    Change mock profile to ya boy Stanley

--- packages/server/src/mocks/getProfile.ts
+++ packages/server/src/mocks/getProfile.ts
@@ -2,10 +2,10 @@ import { GetProfileResponse } from "@template/common";
 
 export const MOCK_GET_PROFILE_RESPONSE: GetProfileResponse = {
   id: 258,
-  email: "stenzel.w@husky.neu.edu",
-  name: "Will Stenzel",
+  email: "liu.sta@husky.neu.edu",
+  name: "Stanley Liu",
   photoURL:
-    "https://prod-web.neu.edu/wasapp/EnterprisePhotoService/PhotoServlet?vid=CCS&er=da30ed4153a22ec0661fe08d52bf29ae29220b5a507f641dcc0a6af3862935bb43b5e2dbadff91cd1e95bd73fb867c75a7a823a4b640ba91",
+    "https://prod-web.neu.edu/wasapp/EnterprisePhotoService/PhotoServlet?vid=CCS&er=d085055dd57706bf9056d0e1b37cc3c23a3a171f4bfb9806e63a74ec5893b57f452cef04303c85953ad9a13953a78c8a875e5405e369d105",
   courses: [
     {
       course: {
