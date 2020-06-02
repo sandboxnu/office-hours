150e6892590cc7458c7c3775925ec7ed73412986
dajinchu <dajinchu@gmail.com>
2020-06-02 00:24:30 -0400

    test error logging

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -7,28 +7,34 @@ import { courseRoutes } from "./api/courseRoutes";
 import { queueRoutes } from "./api/queueRoutes";
 import websocketManager from "./websocketManager";
 
-const server = Hapi.server({
-  port: 3002,
-  host: "localhost",
-});
-
-// Add routes
-server.route(clubRoutes);
-server.route(profileRoutes);
-server.route(courseRoutes);
-server.route(queueRoutes);
-
-// Bind socketio to http server
-websocketManager.bindSocketIO(io(server.listener));
-
-// Good for testing
+// Just initialize, don't start
 export async function init() {
+  const server = Hapi.server({
+    port: 3002,
+    host: "localhost",
+  });
+  // Add routes
+  server.route(clubRoutes);
+  server.route(profileRoutes);
+  server.route(courseRoutes);
+  server.route(queueRoutes);
+
+  // Bind socketio to http server
+  websocketManager.bindSocketIO(io(server.listener));
+
+  await server.register({
+    plugin: require("hapi-dev-errors"),
+    options: {
+      showErrors: process.env.NODE_ENV !== "production",
+    },
+  });
   await server.initialize();
   return server;
 }
 
 // Actually start the server and listen on the port
 export async function start() {
+  const server = await init();
   await server.start();
   console.log("> Server up");
   return server;
