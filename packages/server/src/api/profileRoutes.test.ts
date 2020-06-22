cbb7dd1185d80d9feb2635e593228f85172fcafa
willstenzel <stenzel.will@gmail.com>
2020-06-22 16:50:55 -0700

    Hook up get profile route

--- packages/server/src/api/profileRoutes.test.ts
+++ packages/server/src/api/profileRoutes.test.ts
@@ -1,18 +1,56 @@
-import { setupServerTest, withServer } from "../testUtils";
+import { setupServerTest, withServer, setupDBTest } from "../testUtils";
 import { MOCK_GET_PROFILE_RESPONSE } from "../mocks/getProfile";
+import { UserFactory, CourseFactory } from "../factory";
+import { UserCourseModel } from "../entity/UserCourseModel";
+import { Role } from "@template/common";
 
 describe("Profile Routes", () => {
+  setupDBTest();
   const getServer = setupServerTest();
   const expectWithServer = withServer(getServer);
 
   describe("/profile", () => {
     it("GET profile", async () => {
+      let user = await UserFactory.create();
+      let fundies = await CourseFactory.create({ name: "CS 2500" });
+      await UserCourseModel.create({
+        user,
+        course: fundies,
+        role: Role.STUDENT,
+      }).save();
+      let softwareDevelopemnt = await CourseFactory.create({ name: "CS 4500" });
+      await UserCourseModel.create({
+        user,
+        course: softwareDevelopemnt,
+        role: Role.TA,
+      }).save();
       const get = await getServer().inject({
         method: "get",
         url: "/api/v1/profile",
       });
       expect(get.statusCode).toEqual(200);
-      expect(get.result).toEqual(MOCK_GET_PROFILE_RESPONSE);
+      expect(get.result).toEqual({
+        email: "user1@neu.edu",
+        id: 1,
+        name: "John Doe the 1th",
+        photoURL: "https://pics/1",
+        courses: [
+          {
+            course: {
+              id: 1,
+              name: "CS 2500",
+            },
+            role: "student",
+          },
+          {
+            course: {
+              id: 2,
+              name: "CS 4500",
+            },
+            role: "ta",
+          },
+        ],
+      });
     });
   });
 });
--- packages/server/src/api/profileRoutes.ts
+++ packages/server/src/api/profileRoutes.ts
@@ -1,14 +1,28 @@
 import { ServerRoute } from "@hapi/hapi";
 import { UserSchema } from "../joi";
 import { GetProfileResponse } from "@template/common";
-import { MOCK_GET_PROFILE_RESPONSE } from "../mocks/getProfile";
+import { pick } from "lodash";
+import { UserModel } from "../entity/UserModel";
 
 export const profileRoutes: ServerRoute[] = [
   {
     method: "GET",
     path: "/api/v1/profile",
     handler: async (request, h): Promise<GetProfileResponse> => {
-      return MOCK_GET_PROFILE_RESPONSE;
+      // TODO: Change this to return the specific profile of the user making the request
+      let user = await UserModel.findOne(1, {
+        relations: ["courses", "courses.course"],
+      });
+      user.courses = user.courses.map((userCourse) => {
+        return {
+          course: {
+            id: userCourse.courseId,
+            name: userCourse.course.name,
+          },
+          role: userCourse.role,
+        };
+      });
+      return pick(user, ["id", "email", "name", "photoURL", "courses"]);
     },
     options: {
       response: {
