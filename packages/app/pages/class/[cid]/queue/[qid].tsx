5df609f2724f1d5e11f32cdc78a873a53967e523
aislinblack <1of4black@gmail.com>
2020-06-24 17:53:35 -0400

    Mess with styles?

--- packages/app/pages/class/[cid]/queue/[qid].tsx
+++ packages/app/pages/class/[cid]/queue/[qid].tsx
@@ -164,7 +164,7 @@ export default function Queue({}: QueueProps) {
       // fetch updated question list
       getQuestions();
       setIsJoining(false);
-      setCurrentQuestion(q);
+      setStudentQuestion(q);
     }
   };
 
