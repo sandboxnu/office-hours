e6eef424a0454109ef716aea06471e608f41ac38
Stanley Liu <liustan24@gmail.com>
2020-06-25 15:26:32 -0400

    added profile check to all pages

--- packages/app/pages/queue.tsx
+++ packages/app/pages/queue.tsx
@@ -14,6 +14,7 @@ import StudentPopupCard from "../components/Queue/StudentPopupCard";
 import { useCallback, useState, useContext, useEffect, Fragment } from "react";
 import { API } from "@template/api-client";
 import { ProfileContext } from "../contexts/ProfileContextProvider";
+import { useProfile } from "../hooks/useProfile";
 
 // TODO: replace this with profile role from endpoint
 const ROLE: Role = Role.TA;
@@ -28,7 +29,7 @@ const Container = styled.div`
 interface QueueProps {}
 
 export default function Queue({}: QueueProps) {
-  const { profile } = useContext(ProfileContext);
+  const profile = useProfile();
   const [course, setCourse] = useState<UserCourse>(null);
   const [queueId, setQueueId] = useState<number>(null);
   const [questions, setQuestions] = useState<Question[]>([]);
@@ -180,36 +181,44 @@ export default function Queue({}: QueueProps) {
     // Send API request to trigger notification
   };
 
-  return (
-    <Container>
-      {!isJoining && (
-        <Fragment>
-          <QueueList
-            role={ROLE}
-            onOpenClick={onOpenClick}
-            joinQueue={joinQueue}
-            updateQuestionTA={updateQuestionTA}
-            alertStudent={alertStudent}
-            questions={questions}
-            helpingQuestions={helpingQuestions}
-            groupQuestions={groupQuestions}
-          />
-          {ROLE === "ta" && currentQuestion && (
-            <StudentPopupCard
-              onClose={onCloseClick}
-              email="takayama.a@northeastern.edu" //need a way to access this. or the user
-              wait={20} //figure out later
-              question={currentQuestion}
-              location="Outside by the printer" // need a way to access this
-              visible={openPopup}
-              updateQuestion={updateQuestionTA}
+  // Prevent queue from rendering without authentication
+  if (profile) {
+    return (
+      <Container>
+        {!isJoining && (
+          <Fragment>
+            <QueueList
+              role={ROLE}
+              onOpenClick={onOpenClick}
+              joinQueue={joinQueue}
+              updateQuestionTA={updateQuestionTA}
+              alertStudent={alertStudent}
+              questions={questions}
+              helpingQuestions={helpingQuestions}
+              groupQuestions={groupQuestions}
             />
-          )}
-        </Fragment>
-      )}
-      {isJoining && (
-        <QuestionForm leaveQueue={leaveQueue} finishQuestion={finishQuestion} />
-      )}
-    </Container>
-  );
+            {ROLE === "ta" && currentQuestion && (
+              <StudentPopupCard
+                onClose={onCloseClick}
+                email="takayama.a@northeastern.edu" //need a way to access this. or the user
+                wait={20} //figure out later
+                question={currentQuestion}
+                location="Outside by the printer" // need a way to access this
+                visible={openPopup}
+                updateQuestion={updateQuestionTA}
+              />
+            )}
+          </Fragment>
+        )}
+        {isJoining && (
+          <QuestionForm
+            leaveQueue={leaveQueue}
+            finishQuestion={finishQuestion}
+          />
+        )}
+      </Container>
+    );
+  } else {
+    return null;
+  }
 }
