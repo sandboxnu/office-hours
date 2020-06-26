50e09ee0450f9f087d382c96705d4eedbedd0d93
dajinchu <dajinchu@github.com>
2020-06-25 23:25:52 -0400

    path logging and sql logging

--- ormconfig.js
+++ ormconfig.js
@@ -3,8 +3,8 @@ const path = require("path");
 module.exports = {
   type: "postgres",
   url: process.env.DB_URL || "postgres://postgres@localhost:5432/dev",
-  synchronize: true,
-  logging: false,
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: process.env.NODE_ENV === 'development',
   entities: [
     path.resolve(__dirname, "packages/server/src/entity/**/*{.js,.ts}"),
   ],
