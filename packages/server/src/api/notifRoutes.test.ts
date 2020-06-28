f9fe4b241a461da1d2939fe2efd64196730eb508
NEUDitao <li.edwa@husky.neu.edu>
2020-06-27 20:28:14 -0700

    t a s t

--- packages/server/src/api/notifRoutes.test.ts
+++ packages/server/src/api/notifRoutes.test.ts
@@ -82,12 +82,52 @@ describe("/api/v1/notifications/phone/register/{user_id}", () => {
 
     expect(post.statusCode).toBe(200);
 
-    const notifModels = await PhoneNotifModel.findOne();
-    expect(notifModels).toEqual({
+    const notifModel = await PhoneNotifModel.findOne();
+    expect(notifModel).toEqual({
       id: 1,
       phoneNumber: "+12345678900",
       user: undefined,
       userId: 1,
     });
+  });
+});
+
+describe("/api/v1/notifications/notify_user/{user_id}", () => {
+  setupDBTest();
+  const getServer = setupServerTest();
+
+  it("tests that invalid endpoints for web push get deleted in db", async () => {
+    const user1 = await UserModel.create({
+      username: "ripHapi",
+      email: "ripHapi@bigsad.com",
+      name: "Hapi EOL",
+      photoURL:
+        "https://prod-web.neu.edu/wasapp/EnterprisePhotoService/PhotoServlet?vid=CCS&er=471f2d695fbb8a00ee740ad3ea910453986aec81ddaecf889ae98b3a1858597b12650afd0d4e59c561172f76cb1946eec217ed89bd4074c0",
+    }).save();
+
+    const dateInPayload = new Date();
+
+    const post = await getServer().inject({
+      method: "post",
+      url: `/api/v1/notifications/desktop/register/${user1.id}`,
+      payload: {
+        endpoint: "biggoogle.com",
+        expirationTime: dateInPayload,
+        keys: {
+          p256dh: "some_key",
+          auth: "some_key_as_well",
+        },
+      },
+    });
+
+    expect(post.statusCode).toBe(200);
+
+    await getServer().inject({
+      method: "post",
+      url: `/api/v1/notifications/notify_user/${user1.id}`,
+    });
+
+    const notifModels = await DesktopNotifModel.find();
+    expect(notifModels).toEqual([]);
   });
 });
