3f5c5b831911169780efaae618794d7094927b30
aislinblack <1of4black@gmail.com>
2020-07-03 11:55:36 -0400

    Move profile routes to profile controller

--- packages/nest-server/src/profile/profile.controller.ts
+++ packages/nest-server/src/profile/profile.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Connection } from 'typeorm';
+import { User } from './user.entity';
+import { pick } from 'lodash';
+import { GetProfileResponse } from '@template/common';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+  constructor(private connection: Connection) {}
+
+  @Get()
+  async testing(): Promise<GetProfileResponse> {
+    const user = await User.findOne(1, {
+      relations: ['courses', 'courses.course'],
+    });
+
+    const courses = user.courses.map((userCourse) => {
+      return {
+        course: {
+          id: userCourse.courseId,
+          name: userCourse.course.name,
+        },
+        role: userCourse.role,
+      };
+    });
+
+    const userResponse = pick(user, [
+      'id',
+      'email',
+      'name',
+      'photoURL',
+      'courses',
+    ]);
+    return { ...userResponse, courses };
+  }
+}
