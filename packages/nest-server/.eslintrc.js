3035b8ae3a8e448ccec901e3e49ba8f43a6867e7
dajinchu <dajinchu@github.com>
2020-07-04 14:29:56 -0400

    oopsah

--- packages/nest-server/.eslintrc.js
+++ packages/nest-server/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './packages/**/tsconfig.json',
+    project: './tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
