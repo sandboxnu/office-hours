2afc7e978346315aaf22555bdbf23dd8d04ede35
dajinchu <dajinchu@github.com>
2020-07-09 00:07:50 -0400

    logging

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3002);
 }
 bootstrap();
