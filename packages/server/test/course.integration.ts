f54d594c42238469c13764c2fac2677baff71f23
NEUDitao <li.edwa@husky.neu.edu>
2020-07-16 22:06:27 -0700

    tests

--- packages/server/test/course.integration.ts
+++ packages/server/test/course.integration.ts
@@ -3,6 +3,7 @@ import {
   CourseFactory,
   OfficeHourFactory,
   QueueFactory,
+  StudentCourseFactory,
   TACourseFactory,
   UserFactory,
 } from './util/factories';
@@ -23,6 +24,7 @@ describe('Course Integration', () => {
     });
   });
 
+  //TODO: make a DSL for testing auth points using Hack your own Language
   describe('POST /courses/:id/ta_location/:room', () => {
     it('checks a TA into an existing queue', async () => {
       const queue = await QueueFactory.create();
@@ -37,6 +39,36 @@ describe('Course Integration', () => {
         .expect(201);
 
       expect(response.body).toMatchSnapshot();
+    });
+
+    it("Doesn't allow student to check in", async () => {
+      const queue = await QueueFactory.create();
+      const student = await UserFactory.create();
+      await StudentCourseFactory.create({
+        course: queue.course,
+        user: student,
+      });
+
+      await supertest({ userId: student.id })
+        .post(`/courses/${queue.course.id}/ta_location/${queue.room}`)
+        .expect(500);
+    });
+
+    it('checks TA into a new queue', async () => {
+      const ta = await UserFactory.create();
+      const tcf = await TACourseFactory.create({
+        course: await CourseFactory.create(),
+        user: ta,
+      });
+      const response = await supertest({ userId: ta.id })
+        .post(`/courses/${tcf.courseId}/ta_location/The Alamo`)
+        .expect(201);
+
+      expect(response.body).toMatchObject({
+        id: 1,
+        room: 'The Alamo',
+        staffList: [{ id: ta.id }],
+      });
     });
   });
 });
