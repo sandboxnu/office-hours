d32263a3605e74f7c91689ee4cf7eda931cdcbfb
dajinchu <dajinchu@github.com>
2020-07-18 11:47:52 -0400

    app transpile dependent modules

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,28 +1,9 @@
-const path = require("path");
 const withTM = require("next-transpile-modules");
 const withPlugins = require("next-compose-plugins");
 const withOffline = require("next-offline");
 
-function withCustomWebpack(config = {}) {
-  config.webpack = (config, ...rest) => {
-    // allow other packages to be built with babel
-    const babelRule = config.module.rules.find((rule) =>
-      rule.use && Array.isArray(rule.use)
-        ? rule.use.find((u) => u.loader === "next-babel-loader")
-        : rule.use.loader === "next-babel-loader"
-    );
-    if (babelRule) {
-      babelRule.include.push(path.resolve("../"));
-    }
-    return config;
-  };
-
-  return config;
-}
-
 const plugins = [
-  [withTM, { transpileModules: ["@template"] }],
-  [withCustomWebpack],
+  withTM(["@template"]),
   [withOffline, { dontGenerateSw: true }],
 ];
 
