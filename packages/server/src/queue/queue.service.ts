dc428f0d97d25b5e16834df814b15e66e5ebb190
willstenzel <stenzel.will@gmail.com>
2020-07-25 15:58:48 -0700

    Updates on queue.service plus clean all queues

--- packages/server/src/queue/queue.service.ts
+++ packages/server/src/queue/queue.service.ts
@@ -2,29 +2,47 @@ import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Connection } from 'typeorm';
 import { QueueModel } from './queue.entity';
-import { OpenQuestionStatus } from '@template/common';
+import { OpenQuestionStatus, ClosedQuestionStatus } from '@template/common';
 import { QuestionModel } from '../question/question.entity';
 
 @Injectable()
 export class QueueService {
   constructor(private connection: Connection) {}
 
-  @Cron(CronExpression.EVERY_HOUR)
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  private async cleanAllQueues(): Promise<void> {
+    const queuesWithOpenQuestions: QueueModel[] = await QueueModel.getRepository()
+      .createQueryBuilder('queue')
+      .leftJoinAndSelect('queue_model.questions', 'question')
+      .where('question.status IN (:...status)', {
+        status: Object.values(OpenQuestionStatus),
+      })
+      .getMany();
+
+    queuesWithOpenQuestions.forEach((queue) => {
+      this.cleanQueue(queue.id);
+    });
+  }
+
   public async cleanQueue(queueId: number): Promise<void> {
     const queue = await QueueModel.findOne(queueId, {
-      relations: ['staffList', 'questions'],
+      relations: ['staffList', 'questions', 'officeHours'],
     });
-    // If no staff are present, clean the queue
-    if (!queue.staffList || queue.staffList.length === 0) {
-      const openQuestions = queue.questions.filter((q) =>
-        Object.values(OpenQuestionStatus),
-      );
-      await Promise.all(
-        openQuestions.map(async (q: QuestionModel) => {
-          q.status = 'Stale';
-          await q.save();
-        }),
-      );
+
+    if (!queue.isOpen()) {
+      await this.unsafeClean(queue);
     }
+  }
+
+  private async unsafeClean(queue: QueueModel): Promise<void> {
+    const openQuestions = queue.questions.filter(
+      (q) => q.status in OpenQuestionStatus,
+    );
+
+    openQuestions.forEach((q: QuestionModel) => {
+      q.status = ClosedQuestionStatus.Stale;
+    });
+
+    await QuestionModel.save(openQuestions);
   }
 }
