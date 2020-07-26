4b3e1908ec042aa4ec391065e3e399910e3bfd6a
NEUDitao <li.edwa@husky.neu.edu>
2020-07-26 12:41:17 -0700

    refactored useQueue and useQuestions

--- packages/app/components/Queue/TAQueueList.tsx
+++ packages/app/components/Queue/TAQueueList.tsx
@@ -8,8 +8,9 @@ import {
 import { Button, Card, Col, Grid, Row, Tooltip } from "antd";
 import { ReactElement, useCallback, useState } from "react";
 import styled from "styled-components";
-import useSWR, { mutate } from "swr";
 import { useProfile } from "../../hooks/useProfile";
+import { useQuestions } from "../../hooks/useQuestions";
+import { useQueue } from "../../hooks/useQueue";
 import GroupQuestions from "./GroupQuestions";
 import QueueListHeader from "./QueueListSharedComponents";
 import StudentInfoCard from "./StudentInfoCard";
@@ -123,19 +124,9 @@ export default function TAQueueList({
   const screens = useBreakpoint();
   const user = useProfile();
 
-  const { data: queue, error: queuesError, mutate: mutateQueue } = useSWR(
-    qid && `/api/v1/queues/${qid}`,
-    async () => API.queues.get(Number(qid))
-  );
+  const { queue, queuesError, mutateQueue } = useQueue(qid);
 
-  const { data: questions, error: questionsError } = useSWR(
-    qid && `/api/v1/queues/${qid}/questions`,
-    async () => API.questions.index(Number(qid))
-  );
-
-  const alertStudent = async (question: Question) => {
-    await API.questions.notify(question.id);
-  };
+  const { questions, questionsError, mutateQuestions } = useQuestions(qid);
 
   const helpingQuestions: Question[] = questions?.filter(
     (question) => question.status === OpenQuestionStatus.Helping
@@ -168,7 +159,7 @@ export default function TAQueueList({
     const newQuestions = questions?.map((q) =>
       q.id === question.id ? { ...q, status } : q
     );
-    mutate(`/api/v1/queues/${qid}/questions`, newQuestions);
+    mutateQuestions(newQuestions);
     setOpenPopup(false);
   };
 
@@ -295,7 +286,6 @@ export default function TAQueueList({
             <StudentInfoCard
               key={question.id}
               updateQuestion={updateQuestionTA}
-              alertStudent={alertStudent}
               question={question}
             />
           ))}
