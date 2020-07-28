f16df899f695b92f6f58b5c56128b6d2a504b743
dajinchu <dajinchu@github.com>
2020-07-28 00:52:04 -0400

    format

--- packages/server/ormconfig.ts
+++ packages/server/ormconfig.ts
@@ -1,14 +1,14 @@
-import { CourseModel } from "./src/course/course.entity";
-import { OfficeHourModel } from "./src/course/office-hour.entity";
-import { SemesterModel } from "./src/course/semester.entity";
-import { UserModel } from "./src/profile/user.entity";
-import { UserCourseModel } from "./src/profile/user-course.entity";
-import { QuestionModel } from "./src/question/question.entity";
-import { QueueModel } from "./src/queue/queue.entity";
-import { DesktopNotifModel } from "./src/notification/desktop-notif.entity";
-import { PhoneNotifModel } from "./src/notification/phone-notif.entity";
-import { AdminUserModel } from "./src/admin/admin-user.entity";
-import {config} from 'dotenv';
+import { CourseModel } from './src/course/course.entity';
+import { OfficeHourModel } from './src/course/office-hour.entity';
+import { SemesterModel } from './src/course/semester.entity';
+import { UserModel } from './src/profile/user.entity';
+import { UserCourseModel } from './src/profile/user-course.entity';
+import { QuestionModel } from './src/question/question.entity';
+import { QueueModel } from './src/queue/queue.entity';
+import { DesktopNotifModel } from './src/notification/desktop-notif.entity';
+import { PhoneNotifModel } from './src/notification/phone-notif.entity';
+import { AdminUserModel } from './src/admin/admin-user.entity';
+import { config } from 'dotenv';
 config();
 
 const typeorm = {
