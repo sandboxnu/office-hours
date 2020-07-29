55746453bd79b5c52637250c3fc6745c0f27aac1
Stanley Liu <liustan24@gmail.com>
2020-07-29 00:06:31 -0400

    fix typescript

--- packages/app/pages/course/[cid]/schedule.tsx
+++ packages/app/pages/course/[cid]/schedule.tsx
@@ -1,4 +1,9 @@
-import { Calendar, momentLocalizer, CalendarProps, Event } from "react-big-calendar";
+import {
+  Calendar,
+  momentLocalizer,
+  CalendarProps,
+  Event,
+} from "react-big-calendar";
 import moment from "moment";
 import useSWR from "swr";
 import { API } from "@template/api-client";
@@ -7,6 +12,7 @@ import styled from "styled-components";
 import { useProfile } from "../../../hooks/useProfile";
 import { useRouter } from "next/router";
 import NavBar from "../../../components/Nav/NavBar";
+import { ReactElement } from "react";
 
 const Container = styled.div`
   margin: 32px 64px;
