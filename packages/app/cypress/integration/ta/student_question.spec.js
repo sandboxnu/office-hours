0f7d8a5529e761e705344d400d91455f6fe1de5f
dajinchu <dajinchu@github.com>
2020-08-03 22:38:39 -0400

    student question spec

--- packages/app/cypress/integration/ta/student_question.spec.js
+++ packages/app/cypress/integration/ta/student_question.spec.js
@@ -1,22 +1,33 @@
 describe("TA interacts with student question", () => {
   beforeEach(() => {
     // Setting up the state
-    cy.request('POST', '/api/v1/seeds/createUser', { role: 'ta' }).then(res => res.body).as("ta");
+    cy.request("POST", "/api/v1/seeds/createUser", { role: "ta" })
+      .then((res) => res.body)
+      .as("ta");
 
     cy.get("@ta").then((ta) => {
-      cy.request('POST', '/api/v1/seeds/createQueue', { courseId: ta.course.id }).then(res => res.body).as("queue");
+      cy.request("POST", "/api/v1/seeds/createQueue", {
+        courseId: ta.course.id,
+      })
+        .then((res) => res.body)
+        .as("queue");
     });
-    
+
     cy.get("@ta").then((ta) => {
-        // Login the ta
-        cy.visit(`/api/v1/profile/entry?userId=${ta.user.id}`);
+      // Login the ta
+      cy.request("GET", `/api/v1/profile/entry?userId=${ta.user.id}`);
     });
 
     cy.get("@queue").then((queue) => {
-        // Create a question for the TA to help
-        cy.request('POST', '/api/v1/seeds/createQuestion', { queueId: queue.id }).then(res => res.body);
-        // Check the TA into the queue
-        cy.request('POST', `/api/v1/courses/${queue.course.id}/ta_location/${queue.room}`)
+      // Create a question for the TA to help
+      cy.request("POST", "/api/v1/seeds/createQuestion", {
+        queueId: queue.id,
+      }).then((res) => res.body);
+      // Check the TA into the queue
+      cy.request(
+        "POST",
+        `/api/v1/courses/${queue.course.id}/ta_location/${queue.room}`
+      );
     });
 
     // Visit the queue page
@@ -25,37 +36,39 @@ describe("TA interacts with student question", () => {
     );
   });
 
-  it("clicks the help button then finish helping", () => {  
+  it("clicks the help button then finish helping", () => {
     // Click on the student's question
-    
+    cy.get("[data-cy='ta-queue-card']").should("be.visible").click();
     // Click help
+    cy.get("[data-cy='help-student']").click();
 
     // Click Finish Helping
-
+    cy.contains("button", "Finish Helping").click();
   });
 
   it("clicks help button then mark question as can't find", () => {
     // Click on the student's question
-
+    cy.get("[data-cy='ta-queue-card']").should("be.visible").click();
     // Click help
+    cy.get("[data-cy='help-student']").click();
 
-    // Click Can't Helping
-
+    // Click Finish Helping
+    cy.contains("button", "Can't Find").click();
   });
 
   it("clicks help button then mark question as can't find", () => {
     // Click on the student's question
-
+    cy.get("[data-cy='ta-queue-card']").should("be.visible").click();
     // Click Remove from Queue
-
+    cy.contains("button", "Remove from Queue").click();
     // Click yes on the modal
-
+    cy.contains(".ant-popover-buttons", "Yes").click();
   });
 
-  it("clicks the Help Next button to help the next student", () => {  
+  it("clicks the Help Next button to help the next student", () => {
     // Click on the Help Next button
-    
+    cy.get("[data-cy='help-next']").click();
     // See that the students question is shown as helping
-
+    cy.contains("Helping");
   });
 });
