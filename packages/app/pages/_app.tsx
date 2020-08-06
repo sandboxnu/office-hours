0845dfd0edb378c9f6ec4a8976a755ba5950c0ed
dajinchu <dajinchu@github.com>
2020-08-06 02:48:32 -0400

    only frontend

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -4,13 +4,14 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import "../styles/global.css";
 import { init as initApm } from "@elastic/apm-rum";
 
+if (process.env.NODE_ENV === "production" && typeof window !== "undefined") {
+  initApm({
+    serviceName: `${window.location.hostname.replace(/\./g, "-")}-frontend`,
+    serverUrl: process.env.NEXT_PUBLIC_APM_SERVER,
+  });
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
-  if (process.env.NODE_ENV === "production") {
-    initApm({
-      serviceName: `${window.location.hostname.replace(/\./g, "-")}-frontend`,
-      serverUrl: process.env.NEXT_PUBLIC_APM_SERVER,
-    });
-  }
   return <Component {...pageProps} />;
 }
 
