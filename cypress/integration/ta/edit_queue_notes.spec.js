e2634403e836e1ff7d30acc1a829164513a7c268
willstenzel <stenzel.will@gmail.com>
2020-08-08 11:00:25 -0600

    Move cypress to its own directory

--- cypress/integration/ta/edit_queue_notes.spec.js
+++ cypress/integration/ta/edit_queue_notes.spec.js
@@ -1,6 +1,22 @@
 describe("Edit Queue Notes", () => {
+  beforeEach(() => {
+    // Set the state
+    cy.request("POST", "/api/v1/seeds/createUser", { role: "ta" })
+      .then((res) => res.body)
+      .as("ta");
+    cy.get("@ta").then((ta) => {
+      cy.request("POST", "/api/v1/seeds/createQueue", {
+        courseId: ta.course.id,
+      })
+        .then((res) => res.body)
+        .as("queue");
+
+      // Login the ta
+      cy.visit(`/api/v1/login/dev?userId=${ta.user.id}`);
+    });
+  });
+
   it("can successfully edit queue notes as a ta", () => {
-    cy.login("ta");
     cy.mock("GET", "/api/v1/profile", "fixture:student_profile");
     cy.mock("GET", "/api/v1/courses/1", "fixture:queue_routes_no_notes");
     cy.mock("PATCH", "/api/v1/queues/1", "fixture:queues");
