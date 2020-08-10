ab22ca596ff92619613151c6472ca5ce07738088
dajinchu <dajinchu@github.com>
2020-08-10 01:40:23 -0400

    no sse middleware

--- packages/server/src/sse/sse.module.ts
+++ packages/server/src/sse/sse.module.ts
@@ -1,17 +1,5 @@
-import {
-  Module,
-  NestModule,
-  MiddlewareConsumer,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { SSEService } from './sse.service';
-import { SSEMiddleware } from './sse.middleware';
 
-@Module({ providers: [SSEService, SSEMiddleware], exports: [SSEService] })
-export class SSEModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    // consumer
-    //   .apply(SSEMiddleware)
-    //   .forRoutes({ path: '/queues/:queueId/sse', method: RequestMethod.GET });
-  }
-}
+@Module({ providers: [SSEService], exports: [SSEService] })
+export class SSEModule {}
