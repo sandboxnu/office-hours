a82c314f2c3034e45682e97ff0a318b16868e012
dajinchu <dajinchu@github.com>
2020-08-15 19:35:03 -0400

    fix tests

--- packages/server/src/queue/queue-sse.service.ts
+++ packages/server/src/queue/queue-sse.service.ts
@@ -21,11 +21,14 @@ export class QueueSSEService {
 
   // Send event with new questions, but no more than once a second
   updateQuestions = throttle(
-    async (queueId: number) =>
-      this.sseService.sendEvent(
-        idToRoom(queueId),
-        await this.queueService.getQuestions(queueId),
-      ),
+    async (queueId: number) => {
+      try {
+        this.sseService.sendEvent(
+          idToRoom(queueId),
+          await this.queueService.getQuestions(queueId),
+        );
+      } catch (e) {}
+    },
     1000,
     {
       leading: false,
