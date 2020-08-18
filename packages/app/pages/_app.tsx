087c44d1317c79d857d483af54b6e248b70647b0
dajinchu <dajinchu@github.com>
2020-08-18 13:41:31 -0400

    footer

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -3,6 +3,8 @@ import { AppProps } from "next/app";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "../styles/global.css";
 import { init as initApm } from "@elastic/apm-rum";
+import { Footer } from "../components/common/Footer";
+import styled from "styled-components";
 
 if (process.env.NODE_ENV === "production" && typeof window !== "undefined") {
   initApm({
@@ -11,8 +13,25 @@ if (process.env.NODE_ENV === "production" && typeof window !== "undefined") {
   });
 }
 
+const Layout = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Content = styled.div`
+  flex: 1 0 auto;
+`;
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Content>
+        <Component {...pageProps} />
+      </Content>
+      <Footer />
+    </Layout>
+  );
 }
 
 export default MyApp;
