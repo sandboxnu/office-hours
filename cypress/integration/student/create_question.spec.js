7b442a142041db1704ee315da4dd3d3d321bf812
dajinchu <dajinchu@github.com>
2020-08-17 22:48:53 -0400

    test reliability with .visit.then

--- cypress/integration/student/create_question.spec.js
+++ cypress/integration/student/create_question.spec.js
@@ -18,55 +18,61 @@ describe("Student can create a question", () => {
   it("Create online question", () => {
     // Visit the queue page
     cy.get("@queue").then((queue) =>
-      cy.visit(`/course/${queue.courseId}/queue/${queue.id}`)
+      cy.visit(`/course/${queue.courseId}/queue/${queue.id}`).then(() => {
+        // Click "Join Queue"
+        cy.get("body").should("contain", "Join Queue");
+        cy.get("button").contains("Join Queue").click();
+
+        // Fill out the question form
+        cy.get("body").should("contain", "Concept");
+        cy.get("label").contains("Concept").click({ force: true });
+        cy.get("[data-cy='questionText']").type(
+          "How do I use the design recipe?"
+        );
+        cy.get("label").contains("Online").click();
+
+        // Click Submit
+        cy.get("button").contains("Finish").click();
+
+        // See that the question shows in the queue list
+        cy.get("body").should("contain", "Queued");
+        cy.get("body").contains("John Doe");
+        cy.get("body").contains("Concept");
+        cy.get("body").contains("How do I use the design recipe?");
+        cy.get("body").contains("Online");
+        cy.get("[data-cy='waitTime']").contains("0");
+      })
     );
-
-    // Click "Join Queue"
-    cy.get("body").should("contain", "Join Queue");
-    cy.get("button").contains("Join Queue").click();
-
-    // Fill out the question form
-    cy.get("body").should("contain", "Concept");
-    cy.get("label").contains("Concept").click({ force: true });
-    cy.get("[data-cy='questionText']").type("How do I use the design recipe?");
-    cy.get("label").contains("Online").click();
-
-    // Click Submit
-    cy.get("button").contains("Finish").click();
-
-    // See that the question shows in the queue list
-    cy.get("body").should("contain", "Queued");
-    cy.get("body").contains("John Doe");
-    cy.get("body").contains("Concept");
-    cy.get("body").contains("How do I use the design recipe?");
-    cy.get("body").contains("Online");
-    cy.get("[data-cy='waitTime']").contains("0");
   });
   it("Create in person question", () => {
     // Visit the queue page
     cy.get("@queue").then((queue) =>
-      cy.visit(`/course/${queue.courseId}/queue/${queue.id}`)
+      cy.visit(`/course/${queue.courseId}/queue/${queue.id}`).then(() => {
+        // Click "Join Queue"
+        cy.get("body").should("contain", "Join Queue");
+        cy.get("button").contains("Join Queue").click();
+
+        // Fill out the question form
+        cy.get("body").should("contain", "Concept");
+        cy.get("label").contains("Concept").click({ force: true });
+        cy.get("[data-cy='questionText']").type(
+          "How do I use the design recipe?"
+        );
+        cy.get("[data-cy='locationText']").type(
+          "Outside room, behind the couches"
+        );
+
+        // Click Submit
+        cy.get("button").contains("Finish").click();
+
+        // See that the question shows in the queue list
+        cy.get("body").should("contain", "Queued");
+        cy.get("body").contains("John Doe");
+        cy.get("body").contains("Concept");
+        cy.get("body").contains("How do I use the design recipe?");
+        cy.get("body").contains("Outside room, behind the couches");
+        cy.get("[data-cy='waitTime']").contains("0");
+      })
     );
-
-    // Click "Join Queue"
-    cy.get("body").should("contain", "Join Queue");
-    cy.get("button").contains("Join Queue").click();
-
-    // Fill out the question form
-    cy.get("body").should("contain", "Concept");
-    cy.get("label").contains("Concept").click({ force: true });
-    cy.get("[data-cy='questionText']").type("How do I use the design recipe?");
-    cy.get("[data-cy='locationText']").type("Outside room, behind the couches");
-
-    // Click Submit
-    cy.get("button").contains("Finish").click();
-
-    // See that the question shows in the queue list
-    cy.get("body").should("contain", "Queued");
-    cy.get("body").contains("John Doe");
-    cy.get("body").contains("Concept");
-    cy.get("body").contains("How do I use the design recipe?");
-    cy.get("body").contains("Outside room, behind the couches");
-    cy.get("[data-cy='waitTime']").contains("0");
   });
 });
