6d4884f3a3e2e9e906a742d9ba88196fa11affb3
dajinchu <dajinchu@github.com>
2020-08-29 15:39:18 -0400

    question sse hide sensitive data!

--- packages/server/src/queue/queue-sse.service.ts
+++ packages/server/src/queue/queue-sse.service.ts
@@ -3,6 +3,9 @@ import { SSEService } from 'sse/sse.service';
 import { QueueService } from './queue.service';
 import { Response } from 'express';
 import { throttle } from 'lodash';
+import { Role } from '@template/common';
+
+type QueueClientMetadata = { userId: number; role: Role };
 
 const idToRoom = (queueId: number) => `q-${queueId}`;
 /**
@@ -12,22 +15,28 @@ const idToRoom = (queueId: number) => `q-${queueId}`;
 export class QueueSSEService {
   constructor(
     private queueService: QueueService,
-    private sseService: SSEService,
+    private sseService: SSEService<QueueClientMetadata>,
   ) {}
 
-  subscribeClient(queueId: number, res: Response): void {
-    this.sseService.subscribeClient(idToRoom(queueId), res);
+  subscribeClient(
+    queueId: number,
+    res: Response,
+    metadata: QueueClientMetadata,
+  ): void {
+    this.sseService.subscribeClient(idToRoom(queueId), { res, metadata });
   }
 
   // Send event with new questions, but no more than once a second
   updateQuestions = throttle(
     async (queueId: number) => {
-      const questions = await this.queueService
-        .getQuestions(queueId)
-        .catch((e) => null);
-      if (questions) {
-        this.sseService.sendEvent(idToRoom(queueId), questions);
-      }
+      try {
+        const questions = await this.queueService.getQuestions(queueId);
+        if (questions) {
+          this.sseService.sendEvent(idToRoom(queueId), ({ role, userId }) =>
+            this.queueService.anonymizeQuestions(questions, userId, role),
+          );
+        }
+      } catch (e) {}
     },
     1000,
     {
