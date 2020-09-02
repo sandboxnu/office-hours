3c8b8e6fbd4a38573a9098c42ad214a4bbe918ae
ofloody <oliviafloody@gmail.com>
2020-09-02 17:39:04 -0400

    Update can't find process test. Haven't run yet

---
+++ cypress/integration/student/cant_find.spec.js
@@ -0,0 +1,85 @@
+describe("can't find process", () => {
+  beforeEach(() => {
+    // Set the state
+    cy.request("POST", "/api/v1/seeds/createUser", { role: "student" })
+      .then((res) => res.body)
+      .as("student");
+
+    cy.get("@student").then((student) => {
+      cy.request("POST", "/api/v1/seeds/createQueue", {
+        courseId: student.course.id,
+        allowQuestions: true,
+      })
+        .then((res) => res.body)
+        .as("queue");
+    });
+
+    //creates TA
+    cy.get("@student").then((student) => {
+      cy.request("POST", "/api/v1/seeds/createUser", {
+        role: "ta",
+        courseId: student.course.id,
+      })
+        .then((res) => res.body)
+        .as("ta");
+    });
+
+    // Login the student
+    cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
+
+    // Visit the queue page and create a question
+    cy.get("@queue").then((queue) =>
+      cy.visit(`/course/${queue.courseId}/queue/${queue.id}`).then(() => {
+        // Click "Join Queue"
+        cy.get("body").should("contain", "Join Queue");
+        cy.get("button").contains("Join Queue").click();
+
+        // Fill out the question form
+        cy.get("body").should("contain", "Concept");
+        cy.get("label").contains("Concept").click({
+          force: true,
+        });
+        cy.get("[data-cy='questionText']").type(
+          "How do I use the design recipe?"
+        );
+
+        // Click Submit
+        cy.get("[data-cy='finishQuestion']").click();
+      })
+    );
+
+    cy.get("@queue").then((queue) => {
+      cy.get("@ta").then((ta) => {
+        cy.visit(`/api/v1/login/dev?userId=${ta.user.id}`);
+
+        // Click on the student's question
+        cy.get("[data-cy='ta-queue-card']").should("be.visible").click();
+        // Click help
+        cy.get("[data-cy='help-student']").click();
+
+        // Click Can't Find
+        cy.contains("button", "Can't Find").click();
+
+        cy.request(
+          "DELETE",
+          `/api/v1/courses/${ta.course.id}/ta_location/${queue.room}`
+        );
+      });
+    });
+  });
+
+  it("Process when a TA can't find a student", () => {
+    // Login the student
+    cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
+
+    cy.get("@queue").then((queue) =>
+      cy.visit(`course/${queue.courseId}/queue/${queue.id}`).then(() => {
+        cy.get("body").should(
+          "contain",
+          "A TA tried to help you, but couldn't reach you. Are you still in the\n" +
+            "      queue? If you are, make sure you have Teams open, and rejoin the queue."
+        );
+      })
+    );
+  });
+});
