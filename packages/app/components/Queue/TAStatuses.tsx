d836f6ef3c96a93838e7c7f1a902ffd5f5e3a043
Stanley Liu <liustan24@gmail.com>
2020-09-07 22:11:37 -0400

    filter on helping questions

--- packages/app/components/Queue/TAStatuses.tsx
+++ packages/app/components/Queue/TAStatuses.tsx
@@ -19,7 +19,8 @@ export function TAStatuses({ queueId }: StatusRowProps): ReactElement {
   } = useQueue(queueId);
   const taToQuestion: Record<number, Question> = {};
   const taIds = staffList.map((t) => t.id);
-  for (const question of questions) {
+  const helpingQuestions = questions.filter((q) => q.status === "Helping");
+  for (const question of helpingQuestions) {
     if (taIds.includes(question.taHelped?.id)) {
       taToQuestion[question.taHelped.id] = question;
     }
@@ -114,7 +115,7 @@ function HelpingFor({ studentName, helpedAt }: HelpingForProps): ReactElement {
     <RenderEvery
       render={() => (
         <span>
-          Helping <BlueSpan>{studentName ?? 'a student'}</BlueSpan> for{" "}
+          Helping <BlueSpan>{studentName ?? "a student"}</BlueSpan> for{" "}
           <BlueSpan>
             {Math.round((Date.now() - helpedAt.getTime()) / 60000) + " min"}
           </BlueSpan>
