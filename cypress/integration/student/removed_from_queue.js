cee53c62cbcb9ff30c9fad22161f63a888828e36
willstenzel <stenzel.will@gmail.com>
2020-09-08 10:10:35 -0400

    Rename and update cypress test

--- cypress/integration/student/removed_from_queue.js
+++ cypress/integration/student/removed_from_queue.js
@@ -1,4 +1,4 @@
-describe("Can't find process", () => {
+describe("Removed from queue", () => {
   beforeEach(() => {
     // Set the state
     cy.request("POST", "/api/v1/seeds/createUser", { role: "student" })
@@ -46,7 +46,7 @@ describe("Can't find process", () => {
       cy.visit(`/course/${queue.courseId}/queue/${queue.id}`).then(() => {
         // Click "Join Queue"
         cy.get("body").should("contain", "Join Queue");
-        cy.get("button").contains("Join Queue").click();
+        cy.get('[data-cy="join-queue-button"]').click();
 
         // Fill out the question form
         cy.get("body").should("contain", "Concept");
@@ -70,16 +70,81 @@ describe("Can't find process", () => {
         cy.get(".ant-modal-close-x").click();
         // Visit the queue page
         cy.visit(`/course/${queue.courseId}/queue/${queue.id}`);
-
-        // Click on the student's question
-        cy.get("[data-cy='ta-queue-card']").should("be.visible").click();
-        // Click help
-        cy.get("[data-cy='help-student']").click();
       });
     });
   });
 
-  /*
+
+  it("TA removes student question from the queue and rejoins", () => {
+    cy.get("@queue").then((queue) => {
+      cy.get("@ta").then((ta) => {
+        // Click on the student's question
+        cy.get("[data-cy='ta-queue-card']").should("be.visible").click();
+        // Click help
+        cy.get("[data-cy='help-student']").click();
+        // Click Remove from queue on the Question card
+        cy.get("body").should("contain", "Remove");
+        cy.get("button").contains("Remove").click();
+
+        cy.get("body").should("contain", "Yes");
+        cy.get("button").contains("Yes").click();
+      });
+    });
+
+    // Login the student
+    cy.get("@student").then((student) => {
+      cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
+    });
+    cy.get("@queue").then((queue) =>
+      cy.visit(`course/${queue.courseId}/queue/${queue.id}`).then(() => {
+        cy.get("body").should(
+          "contain",
+          "You've been removed from the queue by a TA. If you have any questions, please reach out to the TA. If you'd like to join back into the queue with your previous question, click Rejoin Queue, otherwise click Leave Queue."
+        );
+
+        cy.get("body").should("contain", "Rejoin Queue");
+        cy.get("button").contains("Rejoin Queue").click();
+
+        // Check that the student was sucessfully but back into the queue
+        cy.contains("You are 1st");
+      })
+    );
+  });
+
+  it("TA removes student question from the queue and leaves", () => {
+    cy.get("@queue").then((queue) => {
+      cy.get("@ta").then((ta) => {
+        // Click on the student's question
+        cy.get("[data-cy='ta-queue-card']").should("be.visible").click();
+        // Click Remove from queue from the sidebar
+        cy.get("body").should("contain", "Remove");
+        cy.get("button").contains("Remove").click();
+
+        cy.get("body").should("contain", "Yes");
+        cy.get("button").contains("Yes").click();
+      });
+    });
+
+    // Login the student
+    cy.get("@student").then((student) => {
+      cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
+    });
+    cy.get("@queue").then((queue) =>
+      cy.visit(`course/${queue.courseId}/queue/${queue.id}`).then(() => {
+        cy.get("body").should(
+          "contain",
+          "You've been removed from the queue by a TA. If you have any questions, please reach out to the TA. If you'd like to join back into the queue with your previous question, click Rejoin Queue, otherwise click Leave Queue."
+        );
+
+        cy.get("body").should("contain", "Leave Queue");
+        cy.get("button").contains("Leave Queue").click();
+        
+        // Check to see that there are no more questions in the queue
+        cy.contains("There currently aren't any questions in the queue");
+      })
+    );
+  });
+    /*
   it("Can't find student and student leaves the queue", () => {
 
     cy.get("@queue").then((queue) => {
@@ -144,61 +209,4 @@ describe("Can't find process", () => {
   });
 */
 
-  it("TA removes student question from the queue and rejoins", () => {
-    cy.get("@queue").then((queue) => {
-      cy.get("@ta").then((ta) => {
-        // Click Remove from queue
-        cy.get("body").should("contain", "Remove");
-        cy.get("button").contains("Remove").click();
-
-        cy.get("body").should("contain", "Yes");
-        cy.get("button").contains("Yes").click();
-      });
-    });
-
-    // Login the student
-    cy.get("@student").then((student) => {
-      cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
-    });
-    cy.get("@queue").then((queue) =>
-      cy.visit(`course/${queue.courseId}/queue/${queue.id}`).then(() => {
-        cy.get("body").should(
-          "contain",
-          "You've been removed from the queue by a TA. If you have any questions, please reach out to the TA. If you'd like to join back into the queue with your previous question, click Rejoin Queue, otherwise click Leave Queue."
-        );
-
-        cy.get("body").should("contain", "Rejoin Queue");
-        cy.get("button").contains("Rejoin Queue").click();
-      })
-    );
-  });
-
-  it("TA removes student question from the queue and leaves", () => {
-    cy.get("@queue").then((queue) => {
-      cy.get("@ta").then((ta) => {
-        // Click Remove from queue
-        cy.get("body").should("contain", "Remove");
-        cy.get("button").contains("Remove").click();
-
-        cy.get("body").should("contain", "Yes");
-        cy.get("button").contains("Yes").click();
-      });
-    });
-
-    // Login the student
-    cy.get("@student").then((student) => {
-      cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
-    });
-    cy.get("@queue").then((queue) =>
-      cy.visit(`course/${queue.courseId}/queue/${queue.id}`).then(() => {
-        cy.get("body").should(
-          "contain",
-          "You've been removed from the queue by a TA. If you have any questions, please reach out to the TA. If you'd like to join back into the queue with your previous question, click Rejoin Queue, otherwise click Leave Queue."
-        );
-
-        cy.get("body").should("contain", "Leave Queue");
-        cy.get("button").contains("Leave Queue").click();
-      })
-    );
-  });
 });
