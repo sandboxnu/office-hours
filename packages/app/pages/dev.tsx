2adc05c68e5889ac064bc9e91f97d94e118162c6
dajinchu <dajinchu@github.com>
2020-09-15 21:46:46 -0400

    test change

--- packages/app/pages/dev.tsx
+++ packages/app/pages/dev.tsx
@@ -61,7 +61,7 @@ export default function DevPanel({ hidePage }: DevPanelProps): ReactElement {
   return (
     <Container>
       <h1>
-        <PageHeader>hello[ For Development Use Only ]</PageHeader>
+        <PageHeader>[ For Development Use Only ]</PageHeader>
       </h1>
       <LoginContainer>
         <Divider plain>
