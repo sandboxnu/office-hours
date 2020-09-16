3ca151fe7962d03b0a7cdf66022114427b025fa6
dajinchu <dajinchu@github.com>
2020-09-16 10:35:02 -0400

    is repo up to date

--- .github/scripts/isRepoUpToDate.sh
+++ .github/scripts/isRepoUpToDate.sh
@@ -1,9 +1,11 @@
+set -e
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
-if [!$LOCAL = $REMOTE && $LOCAL = $BASE]; then
+if [ $LOCAL != $REMOTE ] && [ $LOCAL = $BASE]; then
   UPTODATE="false"
 else 
   UPTODATE="true"
