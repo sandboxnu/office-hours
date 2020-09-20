a4dd5db775d352d764c10b0df3f4f695bb42b52d
dajinchu <dajinchu@github.com>
2020-09-20 16:51:04 -0300

    fix twilio hopefully

--- packages/server/src/notification/notification.controller.ts
+++ packages/server/src/notification/notification.controller.ts
@@ -59,7 +59,7 @@ export class NotificationController {
     const isValidated = twilio.validateRequest(
       twilioAuthToken,
       twilioSignature.trim(),
-      'https://28a88b3af262.ngrok.io/api/v1/notifications/phone/verify',
+      `${this.configService.get('DOMAIN')}/api/v1/notifications/phone/verify`,
       body,
     );
 
