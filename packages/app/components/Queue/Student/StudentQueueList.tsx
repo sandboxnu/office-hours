930e76a0a476db99ab9d127f7c3fd30fff3d2a1d
NEUDitao <edward.d.li@gmail.com>
2020-09-30 13:22:04 -0400

    removed TODO

--- packages/app/components/Queue/Student/StudentQueueList.tsx
+++ packages/app/components/Queue/Student/StudentQueueList.tsx
@@ -152,8 +152,7 @@ export default function StudentQueueList({
           queueId: Number(qid),
           text: "",
           force: force,
-          questionType: QuestionType.Drafting, // TODO: endpoint needs to be changed to allow empty questionType for drafts
-          // for the moment I am defaulting this data so that there is no error
+          questionType: QuestionType.Drafting,
         });
         const newQuestions = [...questions, createdQuestion];
         await mutateQuestions(newQuestions);
