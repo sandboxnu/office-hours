21a616b48c085fac743e3bf459722c3b29ff9635
NEUDitao <edward.d.li@gmail.com>
2020-10-08 17:30:45 -0400

    hehe

--- packages/server/migration/1602086155261-QuestionTypeNullable.ts
+++ packages/server/migration/1602086155261-QuestionTypeNullable.ts
@@ -3,13 +3,13 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class QuestionTypeNullable1602086155261 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE question_model ALTER COLUMN "questionType" DROP NOT NULL;"`,
+      `ALTER TABLE question_model ALTER COLUMN "questionType" DROP NOT NULL;`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE question_model ALTER COLUMN "questionType" SET NOT NULL;"`,
+      `ALTER TABLE question_model ALTER COLUMN "questionType" SET NOT NULL;`,
     );
   }
 }
