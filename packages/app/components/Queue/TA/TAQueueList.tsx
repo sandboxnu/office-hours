f7654068bab87ddb9b9cc2c7be410bd09c56deca
Danish Farooq <>
2020-10-09 12:50:19 -0400

    Notif Reminder button will not appear when the TA is helping a student

--- packages/app/components/Queue/TA/TAQueueList.tsx
+++ packages/app/components/Queue/TA/TAQueueList.tsx
@@ -297,7 +297,7 @@ function QueueQuestions({ questions, isHelping, onOpenCard }: QueueProps) {
       {questions.length === 0 ? (
         <>
           <NoQuestionsText>There are no questions in the queue</NoQuestionsText>
-          {!phoneNotifsEnabled && !desktopNotifsEnabled && (
+          {!isHelping && !phoneNotifsEnabled && !desktopNotifsEnabled && (
             <NotifReminderButton />
           )}
         </>
