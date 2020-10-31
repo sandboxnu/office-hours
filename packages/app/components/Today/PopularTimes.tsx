c5a872c8c25281e323cebb8be29b78bf6c2e4aa3
dajinchu <dajinchu@github.com>
2020-10-30 20:48:59 -0400

    extrract timegraph

--- packages/app/components/Today/PopularTimes.tsx
+++ packages/app/components/Today/PopularTimes.tsx
@@ -1,23 +1,13 @@
-import { DownOutlined, LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { ClockCircleOutlined, DownOutlined, HourglassOutlined, LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { Heatmap } from "@koh/common";
-import { AxisBottom } from "@visx/axis";
-import { Group } from "@visx/group";
-import { scaleBand, scaleLinear } from "@visx/scale";
-import { BarRounded } from "@visx/shape";
-import { defaultStyles } from "@visx/tooltip";
-import useTooltip from "@visx/tooltip/lib/hooks/useTooltip";
-import useTooltipInPortal from "@visx/tooltip/lib/hooks/useTooltipInPortal";
-import { GridRows } from "@visx/grid";
 import { Dropdown, Menu } from "antd";
-import { range } from "lodash";
-import React, { useMemo, useState } from "react";
+import React, { ReactElement, useState } from "react";
 import styled from "styled-components";
-import { formatWaitTime } from "../../utils/TimeUtil";
+import TimeGraph from "./TimeGraph";
 
 // TODO:
-// - Current time labeling?
-// - Min time is weird?
-// - Label Offset
+// - Thursdays have {the shortest} / {shorter than usual} / {average} / {longer than usual} / {the longest} wait times
+// - At {4pm}, people generally wait for {4} hour{s}
 
 const TitleRow = styled.div`
   display: flex;
@@ -39,11 +29,12 @@ const WeekdayDropdown = styled.h2`
 const GraphWithArrow = styled.div`
   display: flex;
   align-items: center;
-  font-size: 1.5em;
+  margin-bottom: 20px;
 `;
 
 const GraphArrowButtons = styled.div`
   padding: 20px 10px;
+  font-size: 1.5em;
   cursor: pointer;
 `;
 
@@ -74,7 +65,13 @@ function findWeekMinAndMax(days: Heatmap) {
   return [minHourInWeek, maxHourInWeek];
 }
 
-export default function PopularTimes({ heatmap }: HeatmapProps) {
+const GraphNotes = styled.h4`
+  font-size: 14px;
+  color: #111;
+  padding-left: 40px;
+`;
+
+export default function PopularTimes({ heatmap }: HeatmapProps): ReactElement {
   const [currentDayOfWeek, setCurrentDayOfWeek] = useState(new Date().getDay());
   const [firstHour, lastHour] = findWeekMinAndMax(heatmap);
   return (
@@ -119,169 +116,18 @@ export default function PopularTimes({ heatmap }: HeatmapProps) {
           <RightOutlined />
         </GraphArrowButtons>
       </GraphWithArrow>
+      <GraphNotes>
+       <HourglassOutlined />
+        {" "}
+        {/* function to generate text */}
+        Today is busier than usual
+      </GraphNotes>
+      <GraphNotes>
+        <ClockCircleOutlined />
+        {" "}
+        {/* function to generate text */}
+        At 4pm, people generally wait {}. 
+      </GraphNotes>
     </div>
-  );
-}
-let tooltipTimeout: number;
-const tooltipStyles = {
-  ...defaultStyles,
-  minWidth: 60,
-  backgroundColor: "rgba(0,0,0,0.9)",
-  color: "white",
-};
-const HORIZONTAL_PADDING = 20;
-const VERTICAL_MARGIN = 40;
-// number of minutes between each grid row line
-const GRID_ROW_INTERVAL = 30;
-const BAR_PADDING = 0.2;
-
-const GraphContainer = styled.div`
-  position: relative;
-
-  & .popularTimes__bar {
-    cursor: pointer;
-  }
-`;
-
-function TimeGraph({
-  values,
-  maxTime,
-  firstHour,
-  lastHour,
-  width,
-  height,
-}: {
-  values: number[];
-  maxTime: number;
-  firstHour: number;
-  lastHour: number;
-  width: number;
-  height: number;
-}) {
-  const {
-    tooltipOpen,
-    tooltipLeft,
-    tooltipTop,
-    tooltipData,
-    hideTooltip,
-    showTooltip,
-  } = useTooltip<number>();
-
-  const { containerRef, TooltipInPortal } = useTooltipInPortal();
-
-  // bounds
-  const xMax = width - HORIZONTAL_PADDING;
-  const yMax = height - VERTICAL_MARGIN;
-
-  // scales, memoize for performance
-  const xScale = useMemo(
-    () =>
-      scaleBand<number>({
-        range: [0, xMax],
-        round: true,
-        domain: range(Math.max(0, firstHour), Math.min(lastHour + 1, 24)),
-        padding: BAR_PADDING,
-      }),
-    [xMax, firstHour, lastHour]
-  );
-  const yScale = useMemo(
-    () =>
-      scaleLinear<number>({
-        range: [yMax, 0],
-        round: true,
-        domain: [0, maxTime],
-      }),
-    [yMax, maxTime]
-  );
-  const barWidth = xScale.bandwidth();
-
-  const LEFT_PADDING = HORIZONTAL_PADDING / 2 - barWidth / 2;
-
-  return width < 10 ? null : (
-    // relative position is needed for correct tooltip positioning
-    <GraphContainer>
-      <svg ref={containerRef} width={width} height={height}>
-        <rect
-          x={0}
-          y={0}
-          width={width}
-          height={height}
-          fill="rgba(0,0,0,0)"
-          rx={14}
-        />
-        <GridRows
-          top={VERTICAL_MARGIN / 2}
-          width={width}
-          scale={yScale}
-          tickValues={range(0, maxTime, GRID_ROW_INTERVAL)}
-          stroke="#cccccc"
-        />
-        <Group left={LEFT_PADDING} top={VERTICAL_MARGIN / 2}>
-          {values.map((value, i) => {
-            const barHeight = yMax - yScale(value);
-            const barX = xScale(i) + (barWidth * (1 + BAR_PADDING)) / 2;
-            const barY = yMax - barHeight;
-            return (
-              <BarRounded
-                key={`bar-${value}`}
-                className="popularTimes__bar"
-                x={barX}
-                y={barY}
-                width={barWidth}
-                height={barHeight}
-                radius={20}
-                top
-                fill="#40a9ff"
-                onMouseLeave={() => {
-                  tooltipTimeout = window.setTimeout(() => {
-                    hideTooltip();
-                  }, 300);
-                }}
-                onMouseDown={() => {
-                  if (tooltipTimeout) clearTimeout(tooltipTimeout);
-                  const top = yMax - barHeight; // - VERTICAL_MARGIN - barHeight;
-                  const left = barX + barWidth / 2;
-                  showTooltip({
-                    tooltipData: value,
-                    tooltipTop: top,
-                    tooltipLeft: left,
-                  });
-                }}
-              />
-            );
-          })}
-        </Group>
-        <Group left={LEFT_PADDING}>
-          <AxisBottom
-            top={yMax + VERTICAL_MARGIN / 2}
-            rangePadding={HORIZONTAL_PADDING + barWidth}
-            scale={xScale}
-            tickFormat={(hour: number) =>
-              (hour - firstHour) % 3 == 0
-                ? hour < 12
-                  ? `${hour + 1}AM`
-                  : `${hour - 11}PM`
-                : ""
-            }
-            tickLabelProps={() => ({
-              fill: "",
-              fontSize: 11,
-              textAnchor: "middle",
-            })}
-          />
-        </Group>
-      </svg>
-
-      {tooltipOpen && tooltipData && (
-        <TooltipInPortal
-          key={Math.random()} // update tooltip bounds each render
-          top={tooltipTop}
-          left={tooltipLeft}
-          style={tooltipStyles}
-        >
-          {formatWaitTime(Math.floor(tooltipData))}
-        </TooltipInPortal>
-      )}
-    </GraphContainer>
   );
 }
