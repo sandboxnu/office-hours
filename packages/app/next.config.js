68e14a42a1a2c452d90a265865caaffdfd9edd3d
dajinchu <dajinchu@users.noreply.github.com>
2020-11-08 16:53:16 +0000

    Prettified Code!

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,4 +1,6 @@
-const withTM = require("next-transpile-modules")(["@koh"], {unstable_webpack5: true});
+const withTM = require("next-transpile-modules")(["@koh"], {
+  unstable_webpack5: true,
+});
 const withPlugins = require("next-compose-plugins");
 const withOffline = require("next-offline");
 
