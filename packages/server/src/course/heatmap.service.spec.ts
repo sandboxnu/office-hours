450dbd5751e53cc3c917014bbec10ca7acdf725b
dajinchu <dajinchu@github.com>
2020-11-08 20:16:08 -0500

    heatmap tests

--- packages/server/src/course/heatmap.service.spec.ts
+++ packages/server/src/course/heatmap.service.spec.ts
@@ -1,10 +1,11 @@
-import { ClosedQuestionStatus } from '@koh/common';
+import { ClosedQuestionStatus, Heatmap } from '@koh/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import moment = require('moment');
 import { QuestionModel } from 'question/question.entity';
 import { Connection } from 'typeorm';
 import { TestTypeOrmModule } from '../../test/util/testUtils';
 import { HeatmapService } from './heatmap.service';
+import { OfficeHourModel } from './office-hour.entity';
 
 describe('HeatmapService', () => {
   let service: HeatmapService;
@@ -25,51 +26,113 @@ describe('HeatmapService', () => {
   });
 
   describe('generateHeatmap with replay', () => {
-    function resolvedQuestion(createdAt: Date, helpedAt: Date): QuestionModel {
-      return QuestionModel.create({
-        createdAt,
-        helpedAt,
-        text: 'pls help',
-        status: ClosedQuestionStatus.Resolved,
-      });
-    }
     // Return a list of question models from a list of ISO String pairs representing start and end times
-    function resolvedQuestions(
+    function questionsFromDates(
       questionTimes: [string, string][],
     ): QuestionModel[] {
       return questionTimes.map(([createdAt, helpedAt]) =>
-        resolvedQuestion(new Date(createdAt), new Date(helpedAt)),
+        QuestionModel.create({
+          createdAt: new Date(createdAt),
+          helpedAt: new Date(helpedAt),
+          text: 'pls help',
+          status: ClosedQuestionStatus.Resolved,
+        }),
       );
     }
 
-    const emptyHeatmap = () => [...Array(24 * 7)].map(() => -1);
+    // Return a list of office hour models from a list of ISO String pairs representing start and end times
+    function officehoursFromDates(
+      hoursTimes: [string, string][],
+    ): OfficeHourModel[] {
+      return hoursTimes.map(([start, end]) =>
+        OfficeHourModel.create({
+          startTime: new Date(start),
+          endTime: new Date(end),
+        }),
+      );
+    }
+
+    // OCT 8 is a Thursday
+    const OCT8 = (start: string, end: string): [string, string] => [
+      `2020-10-04T${start}:00.000Z`,
+      `2020-10-04T${end}:00.000Z`,
+    ];
+
+    // OCT 4 is a Sunday
+    const OCT4 = (start: string, end: string): [string, string] => [
+      `2020-10-04T${start}:00.000Z`,
+      `2020-10-04T${end}:00.000Z`,
+    ];
+
+    // OCT 11 is a Sunday
+    const OCT11 = (start: string, end: string): [string, string] => [
+      `2020-10-11T${start}:00.000Z`,
+      `2020-10-11T${end}:00.000Z`,
+    ];
+
+    // OCT 18 is a Sunday
+    const OCT18 = (start: string, end: string): [string, string] => [
+      `2020-10-18T${start}:00.000Z`,
+      `2020-10-18T${end}:00.000Z`,
+    ];
 
     const BUCKET_SIZE = 60;
     const SAMPLES_PER_BUCKET = 4;
     // Sample timepoints for bucket N are at N:00, N:15, N:30, N:45
 
-    it('returns heatmap for just one question', () => {
-      const questions = resolvedQuestions([
-        // 10-04 is Sunday
-        ['2020-10-04T03:01:00.000Z', '2020-10-04T03:16:00.000Z'],
-      ]);
-      const heatmap = service._generateHeatMapWithReplay(questions, 60, 4);
+    // Generate a heatmap from the historical question times and office hours times
+    function heatmapFromDates(
+      questionTimes: [string, string][],
+      hoursTimes: [string, string][],
+    ): Heatmap {
+      return service._generateHeatMapWithReplay(
+        questionsFromDates(questionTimes),
+        officehoursFromDates(hoursTimes),
+        'America/New_York',
+        BUCKET_SIZE,
+        SAMPLES_PER_BUCKET,
+      );
+    }
+
+    const emptyHeatmap = () => [...Array(24 * 7)].map(() => -1);
+    const sparseHeatmap = (records: Record<number, number>) =>
+      Object.assign(emptyHeatmap(), records);
+
+    it('returns heatmap for no questions and no hours', () => {
+      const heatmap = heatmapFromDates([], []);
       const expected = emptyHeatmap();
-      expected[3] = 1 / 4;
+      expect(heatmap).toEqual(expected);
+    });
+
+    it('returns heatmap for no questions during office hours', () => {
+      const heatmap = heatmapFromDates([], [OCT4('03:00', '04:00')]);
+      const expected = sparseHeatmap({ 3: 0 });
+      expect(heatmap).toEqual(expected);
+    });
+
+    it('returns heatmap for just one question', () => {
+      const heatmap = heatmapFromDates(
+        [OCT4('03:01', '03:16')],
+        [OCT4('03:00', '04:00')],
+      );
+      const expected = sparseHeatmap({ 3: 1 / 4 });
       expect(heatmap).toEqual(expected);
     });
 
     it('returns heatmap when questions overlap within 1 bucket', () => {
-      const questions = resolvedQuestions([
-        // 10-04 is Sunday
-        ['2020-10-04T02:59:00.000Z', '2020-10-04T03:10:00.000Z'], // spans first timepoint
-        ['2020-10-04T03:05:00.000Z', '2020-10-04T03:21:00.000Z'], // spans second timepoint
-        ['2020-10-04T03:10:00.000Z', '2020-10-04T03:31:00.000Z'], // spans second and third timepoint
-        ['2020-10-04T03:48:00.000Z', '2020-10-04T03:58:00.000Z'], // spans third timepoint
-      ]);
+      const heatmap = heatmapFromDates(
+        [
+          OCT4('02:59', '03:10'), // spans first timepoint
+          OCT4('03:05', '03:21'), // spans second timepoint
+          OCT4('03:10', '03:31'), // spans second and third timepoint
+          OCT4('03:48', '03:58'), // spans third timepoint
+        ],
+        [OCT4('03:00', '04:00')],
+      );
       /**
        *   Timepoint | Wait time   | Question in front of you
        *   -----------------------------------------------------
+       *     2:45    | 0 min       |
        *     3:00    | 10 minutes  |  Q1
        *     3:15    | 16 mintues  |  Q3
        *     3:30    | 1 minutes   |  Q3
@@ -77,24 +140,54 @@ describe('HeatmapService', () => {
        *
        *  Average timepoint wait = 6.75
        */
-      const heatmap = service._generateHeatMapWithReplay(questions, 60, 4);
-      const expected = emptyHeatmap();
-      expected[3] = 6.75;
+      const expected = sparseHeatmap({
+        3: 6.75,
+      });
       expect(heatmap).toEqual(expected);
     });
 
-    it('returns heatmap when questions overlap spanning multiple bucket', () => {
-      const questions = resolvedQuestions([
-        // 10-04 is Sunday
-        ['2020-10-04T03:30:00.000Z', '2020-10-04T04:10:00.000Z'], // spans two buckets
-        ['2020-10-04T04:01:00.000Z', '2020-10-04T04:21:00.000Z'], // spans first 4 o clock bucket
-        ['2020-10-04T04:05:00.000Z', '2020-10-04T04:16:00.000Z'], // somehow gets help before the previous guy
-      ]);
+    it('returns heatmap when questions overlap spanning multiple buckets', () => {
+      const heatmap = heatmapFromDates(
+        [
+          OCT4('03:30', '04:10'), // spans two buckets
+          OCT4('04:01', '04:21'), // spans first 4 o clock bucket
+        ],
+        [OCT4('03:00', '05:00')],
+      );
       /**
        *   Timepoint | Wait time   | Question in front of you
        *   -----------------------------------------------------
-       *     3:00    |  0 minutes  |    
-       *     3:15    |  0 mintues  |    
+       *     3:00    |  0 minutes  |
+       *     3:15    |  0 mintues  |
+       *     3:30    | 40 minutes  |  Q1
+       *     3:45    | 25 minutes  |  Q1
+       *     4:00    | 10 minutes  |  Q1
+       *     4:15    |  6 minutes  |  Q2
+       *
+       *  3-4 bucket avg: 16.25
+       *  4-5 bucket avg: 4
+       */
+      const expected = sparseHeatmap({
+        3: 16.25,
+        4: 4,
+      });
+      expect(heatmap).toEqual(expected);
+    });
+
+    it('returns heatmap when questions overlap spanning multiple buckets and a question is skipped', () => {
+      const heatmap = heatmapFromDates(
+        [
+          OCT4('03:30', '04:10'), // spans two buckets
+          OCT4('04:01', '04:21'), // spans first 4 o clock bucket
+          OCT4('04:05', '04:16'), // somehow gets help before the previous guy
+        ],
+        [OCT4('03:00', '05:00')],
+      );
+      /**
+       *   Timepoint | Wait time   | Question in front of you
+       *   -----------------------------------------------------
+       *     3:00    |  0 minutes  |
+       *     3:15    |  0 mintues  |
        *     3:30    | 40 minutes  |  Q1
        *     3:45    | 25 minutes  |  Q1
        *     4:00    | 10 minutes  |  Q1
@@ -103,27 +196,295 @@ describe('HeatmapService', () => {
        *  3-4 bucket avg: 16.25
        *  4-5 bucket avg: 2.75
        */
-      const heatmap = service._generateHeatMapWithReplay(questions, 60, 4);
-      const expected = emptyHeatmap();
-      expected[3] = 16.25;
-      expected[4] = 2.75;
+      const expected = sparseHeatmap({
+        3: 16.25,
+        4: 2.75,
+      });
       expect(heatmap).toEqual(expected);
     });
 
     it('returns heatmap when large gap between questions', () => {
-      const questions = resolvedQuestions([
-        // 10-04 is Sunday
-        ['2020-10-04T03:30:00.000Z', '2020-10-04T04:10:00.000Z'], // spans two buckets
-        ['2020-10-04T04:01:00.000Z', '2020-10-04T04:21:00.000Z'], // spans first 4 o clock bucket
-        ['2020-10-04T04:05:00.000Z', '2020-10-04T04:16:00.000Z'], // somehow gets help before the previous guy
-      ]);
-
+      const heatmap = heatmapFromDates(
+        [
+          OCT4('03:10', '03:30'),
+          OCT4('07:01', '07:21'), // much later question
+        ],
+        [OCT4('03:00', '04:00'), OCT4('07:00', '08:00')],
+      );
+      /**
+       *   Timepoint | Wait time   | Question in front of you
+       *   -----------------------------------------------------
+       *     3:00    |  0 minutes  |
+       *     3:15    | 15 mintues  |  Q1
+       *     3:30    |  0 minutes  |
+       *     3:45    |  0 minutes  |
+       *
+       *     7:00    |  0 minutes  |
+       *     7:15    |  6 minutes  |  Q2
+       *
+       *  3-4 bucket avg: 15/4
+       *  7-8 bucket avg: 6/4
+       */
+      const expected = sparseHeatmap({
+        3: 15 / 4,
+        7: 6 / 4,
+      });
+      expect(heatmap).toEqual(expected);
     });
 
-    it('returns heatmap when questions are later in the week', () => {});
+    it('returns heatmap where a question is asked outside of scheduled office hours', () => {
+      const heatmap = heatmapFromDates(
+        [OCT4('03:10', '03:30')],
+        [OCT4('07:00', '08:00')],
+      );
+      const expected = sparseHeatmap({
+        7: 0,
+      });
+      expect(heatmap).toEqual(expected);
+    });
 
-    it('returns heatmap when questions are across multiple weeks', () => {});
+    it('returns heatmap when questions are later in the week', () => {
+      const heatmap = heatmapFromDates(
+        [
+          OCT4('03:30', '04:10'),
+          OCT4('04:01', '04:21'),
+          // Thursday
+          OCT8('03:30', '04:10'),
+          OCT8('04:01', '04:21'),
+        ],
+        [OCT4('03:00', '05:00'), OCT8('03:00', '05:00')],
+      );
+      const expected = sparseHeatmap({
+        3: 16.25,
+        4: 2.5,
+        [24 * 3 + 3]: 16.25,
+        [24 * 3 + 3]: 2.5,
+      });
 
-    it('returns heatmap during a week with daylight savings transition', () => {});
+      expect(expected).toEqual(heatmap);
+    });
+
+    it('returns heatmap when questions are across multiple weeks', () => {
+      // avg of the office hours results
+      const heatmap = heatmapFromDates(
+        [
+          OCT4('03:30', '04:10'),
+          OCT4('04:01', '04:21'),
+
+          OCT11('03:21', '04:25'),
+          OCT11('04:22', '04:55'),
+        ],
+        [OCT4('03:00', '05:00'), OCT11('03:00', '05:00')],
+      );
+      /**
+       *   Timepoint | Wait time   | Question in front of you
+       *   -----------------------------------------------------
+       *     3:00    |  0 minutes  |
+       *     3:15    |  0 mintues  |
+       *     3:30    | 40 minutes  |  Q1
+       *     3:45    | 25 minutes  |  Q1
+       *     4:00    | 10 minutes  |  Q1
+       *     4:15    |  6 minutes  |  Q2
+       *
+       *  3-4 bucket avg: 16.25
+       *  4-5 bucket avg: 4
+       *
+       *
+       *    Timepoint | Wait time   | Question in front of you
+       *   -----------------------------------------------------
+       *     3:00    |  0 minutes  |
+       *     3:15    |  0 mintues  |
+       *     3:30    | 55 minutes  |  Q1
+       *     3:45    | 40 minutes  |  Q1
+       *     4:00    | 25 minutes  |  Q1
+       *     4:15    | 10 minutes  |  Q2
+       *     4:30    | 25 minutes  |
+       *     4:45    | 10 minutes  |  Q2
+       *     5:00    |  0 minutes  |  Q2
+       *
+       *  3-4 bucket avg: 23.75
+       *  4-5 bucket avg: 17.5
+       *
+       * ------OVERALL-----
+       * 3-4 bucket avg: 20
+       * 4-5 bucket avg: 10.75
+       */
+
+      const expected = sparseHeatmap({
+        3: 20,
+        4: 10.75,
+      });
+      expect(heatmap).toEqual(expected);
+    });
+
+    it('returns heatmap when questions are across multiple weeks but no office hours during one of the weeks', () => {
+      // avg of the office hours results, ignore any -1s and divide accordingly (to get the right avg)
+      const heatmap = heatmapFromDates(
+        [OCT4('03:30', '04:10'), OCT18('03:21', '04:25')],
+        [OCT4('03:00', '45:00'), OCT18('03:00', '04:00')],
+      );
+      /**
+       *   Timepoint | Wait time   | Question in front of you
+       *   -----------------------------------------------------
+       *     3:00    |  0 minutes  |
+       *     3:15    |  0 mintues  |
+       *     3:30    | 40 minutes  |  Q1
+       *     3:45    | 25 minutes  |  Q1
+       *
+       *  3-4 bucket avg: 16.25
+       *
+       *    Timepoint | Wait time   | Question in front of you
+       *   -----------------------------------------------------
+       *     3:00    |  0 minutes  |
+       *     3:15    |  0 mintues  |
+       *     3:30    | 55 minutes  |  Q1
+       *     3:45    | 40 minutes  |  Q1
+       *
+       *  3-4 bucket avg: 23.75
+       *
+       * ------OVERALL-----
+       * 3-4 bucket avg: 20
+       */
+
+      const expected = sparseHeatmap({
+        3: 20,
+      });
+      expect(heatmap).toEqual(expected);
+    });
+
+    it('returns heatmap during a week with daylight savings fall back', () => {
+      // while course is in America/New_York
+      const heatmap = heatmapFromDates(
+        [
+          [`2020-10-25T08:04.000Z`, `2020-10-25T08:32.000Z`],
+          [`2020-11-01T09:04.000Z`, `2020-10-25T09:32.000Z`],
+        ],
+        [
+          [`2020-10-25T08:00.000Z`, `2020-10-25T08:00.000Z`],
+          [`2020-11-01T09:00.000Z`, `2020-11-01T09:00.000Z`], // same office hour after falling back
+        ],
+      );
+      /**
+       * For both weeks wait time is 19/4
+       */
+
+      const expected = sparseHeatmap({
+        8: 19 / 4,
+      });
+      expect(expected).toEqual(heatmap);
+    });
+
+    it('returns heatmap during a week with daylight savings, but the course is not in a DST timezone', () => {
+      // Honolulu doesn't have DST (Pacific/Honolulu)
+      const heatmap = service._generateHeatMapWithReplay(
+        questionsFromDates([
+          [`2020-10-25T08:04.000Z`, `2020-10-25T08:32.000Z`],
+          [`2020-11-01T09:04.000Z`, `2020-10-25T09:32.000Z`],
+        ]),
+        officehoursFromDates([
+          [`2020-10-25T08:00.000Z`, `2020-10-25T08:00.000Z`],
+          [`2020-11-01T09:00.000Z`, `2020-11-01T09:00.000Z`], // same office hour after falling back
+        ]),
+        'Pacific/Honolulu',
+        BUCKET_SIZE,
+        SAMPLES_PER_BUCKET,
+      );
+      /**
+       * For both weeks wait time is 19/4
+       */
+
+      const expected = sparseHeatmap({
+        8: 19 / 4,
+        9: 19 / 4,
+      });
+      expect(expected).toEqual(heatmap);
+    });
+
+    it('returns heatmap during a week with daylight savings spring forward', () => {
+      // while course is in America/New_York
+      const heatmap = heatmapFromDates(
+        [
+          [`2020-03-07T08:04.000Z`, `2020-03-07T08:32.000Z`],
+          [`2020-03-14T07:04.000Z`, `2020-03-14T07:32.000Z`],
+        ],
+        [
+          [`2020-03-07T08:00.000Z`, `2020-03-07T08:00.000Z`],
+          [`2020-03-14T07:00.000Z`, `2020-03-14T07:00.000Z`], // same office hour after spring forward
+        ],
+      );
+      /**
+       * For both weeks wait time is 19/4
+       */
+
+      const expected = sparseHeatmap({
+        8: 19 / 4,
+      });
+      expect(expected).toEqual(heatmap);
+    });
+
+    it('returns heatmap during fall back with question crossing boundary', () => {
+      // while course is in America/New_York
+      const heatmap = heatmapFromDates(
+        [
+          [`2020-11-01T05:44.000Z`, `2020-11-01T06:12.000Z`], // 1:44 EDT -> 1:12 EST
+        ],
+        [[`2020-11-01T05:00.000Z`, `2020-11-01T07:00.000Z`]],
+      );
+      /**
+       *   Timepoint | Wait time   | Question in front of you
+       *   -----------------------------------------------------
+       *     1:45    | 28 minutes  | Q1
+       *
+       *     1:00    | 12 mintues  | Q1
+       *
+       * we have to divide by 8 because this hour happened twice
+       **/
+
+      const expected = sparseHeatmap({
+        1: 40 / 8,
+      });
+      expect(expected).toEqual(heatmap);
+    });
+
+    it('returns heatmap during spring forward with question crossing boundary', () => {
+      // while course is in America/New_York
+      const heatmap = heatmapFromDates(
+        [
+          [`2020-03-08T06:44.000Z`, `2020-03-08T07:12.000Z`], // 1:44 EST -> 3:12 EDT
+        ],
+        [[`2020-03-08T06:00.000Z`, `2020-03-07T08:00.000Z`]],
+      );
+      /**
+       *   Timepoint | Wait time   | Question in front of you
+       *   -----------------------------------------------------
+       *     1:45    | 28 minutes  | Q1
+       *
+       *     3:00    | 12 mintues  | Q1
+       *
+       *
+       *   We don't bucket for 2AM at all.
+       **/
+
+      const expected = sparseHeatmap({
+        1: 28 / 4,
+        3: 12 / 4,
+      });
+      expect(expected).toEqual(heatmap);
+    });
+
+    it('returns heatmap when a question crosses the midnight boundary', () => {
+      // should be completely ignored
+      const heatmap = heatmapFromDates(
+        [[`2020-10-11T05:00.000Z`, `2020-10-12T02:00.000Z`]],
+        [OCT4('03:00', '04:00')],
+      );
+
+      const expected = sparseHeatmap({
+        3: 0,
+      });
+      expect(expected).toEqual(heatmap);
+    });
+
+    it('works when the bucketsize and sample interval are different', () => {});
   });
 });
