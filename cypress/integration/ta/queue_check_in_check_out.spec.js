2a0461db2640604285d9e6301326d32372d7a2eb
willstenzel <stenzel.will@gmail.com>
2020-11-12 22:37:49 -0600

    Wrap up last check in check out test

--- cypress/integration/ta/queue_check_in_check_out.spec.js
+++ cypress/integration/ta/queue_check_in_check_out.spec.js
@@ -1,6 +1,6 @@
 import { loginUser, checkInTA, createQueue, createQuestion } from "../utils";
 
-describe("Can successfuly check in and out of a queue", () => {
+describe("Can successfuly check in and out of a queue when their is scheduled office hours", () => {
   beforeEach(() => {
     // Set the state
     loginUser({
@@ -26,22 +26,21 @@ describe("Can successfuly check in and out of a queue", () => {
     });
     createQuestion({
       queueId: "queue.id",
+      identifier: "question", // TODO: Remove this once we have default values
     });
 
     cy.get(".ant-modal-close-x").click();
     // Click "Check in"
     cy.get("[data-cy='check-in-button']").click();
-
     cy.location("pathname").should("contain", "/queue");
-    cy.get("body").should("contain", "There are no questions in the queue");
 
     // Wait to see that the user has been checked in
     cy.contains("Check Out");
     cy.get("[data-cy='ta-status-card']").should("have.length", "2");
     cy.percySnapshot("TA Queue Page - Two TA's Checked In");
 
-    // 1 student should be in the queu
-    cy.get("[data-cy='ta-queue-card]").should("have.length", "1");
+    // 1 student should be in the queue
+    cy.get("[data-cy='ta-queue-card']").should("have.length", "1");
 
     // Click "Check out"
     cy.get("[data-cy='check-out-button']").click();
@@ -49,7 +48,7 @@ describe("Can successfuly check in and out of a queue", () => {
 
     // Other TA should still be checked in, and there should still be 1 student in the queue
     cy.get("[data-cy='ta-status-card']").should("have.length", "1");
-    cy.get("[data-cy='ta-queue-card]").should("have.length", "1");
+    cy.get("[data-cy='ta-queue-card']").should("have.length", "1");
     cy.percySnapshot("TA Queue Page - One TA Checked out One TA Checked In");
   });
 
@@ -95,8 +94,6 @@ describe("Can successfuly check in and out of a queue", () => {
   });
 });
 
-// future tests: add one where there are more TAs than one, add one where
-// there are more than 1 office hour at a time
 describe("Checking in and out when there arent scheduled office hours", () => {
   beforeEach(() => {
     // Set the state
@@ -106,32 +103,35 @@ describe("Checking in and out when there arent scheduled office hours", () => {
     });
   });
 
-  it("checking in multiple TAs when there is not a scheduled office hours", function () {
+  it("checking in a TA when there is not scheduled office hours", function () {
     // check into the queue
     cy.request({
       method: "POST",
-      url: `/api/v1/courses/${this.ta.courseId}/ta_location/Online}`,
+      url: `/api/v1/courses/${this.ta.courseId}/ta_location/Online`,
     })
       .then((res) => res.body)
-      .as("@queue");
+      .as("queue")
+      .then((queue) => {
+        // add a question to the queue
+        createQuestion({
+          queueId: "queue.id",
+          identifier: "question",
+        });
+        // Navigate to the queue page
+        cy.visit(`/course/${this.ta.courseId}/queue/${queue.id}`);
+      });
 
-    // add a question to the queue
-    createQuestion({
-      queueId: "queue.id",
-    });
-
-    // Navigate to the queue page
-    cy.visit(`/course/${this.queue.courseId}/queue/${this.queue.id}`);
-
+    // The ta should show as checked in
+    cy.get("[data-cy='ta-status-card']").should("be.visible");
     // 1 student should be in the queue
-    cy.get("[data-cy='ta-queue-card]").should("have.length", "1");
+    cy.get("[data-cy='ta-queue-card']").should("have.length", "1");
 
     // Click "Check out"
     cy.get("[data-cy='check-out-button']").click();
     cy.get("button").should("contain", "Check In");
 
     // No TAs should be checked in, and there should not be any student in the queue
-    cy.get("[data-cy='ta-status-card']").should("have.length", "0");
-    cy.get("[data-cy='ta-queue-card]").should("have.length", "0");
+    cy.get("[data-cy='ta-status-card']").should("not.visible");
+    cy.get("[data-cy='ta-queue-card']").should("not.visible");
   });
 });
