7b1cf739cceabc9db3b31647c47b8bcbe6c95831
NEUDitao <edward.d.li@gmail.com>
2020-11-15 15:28:14 -0500

    student/cant_find_tests are now newly cypress-ized

--- cypress/integration/student/cant_find_test.spec.js
+++ cypress/integration/student/cant_find_test.spec.js
@@ -1,66 +1,31 @@
+const {
+  createQueue,
+  checkInTA,
+  createQuestion,
+  loginStudent,
+  loginTA,
+} = require("../../utils");
+
 describe("can't be found", () => {
   beforeEach(() => {
-    // Set the state
-    cy.request("POST", "/api/v1/seeds/createUser", { role: "student" })
-      .then((res) => res.body)
-      .as("student");
-
-    cy.get("@student").then((student) => {
-      cy.request("POST", "/api/v1/seeds/createQueue", {
-        courseId: student.course.id,
-        allowQuestions: true,
-      })
-        .then((res) => res.body)
-        .as("queue");
+    loginStudent();
+    createQueue({
+      courseId: "student.course.id",
     });
-
-    //creates TA
-    cy.get("@student").then((student) => {
-      cy.request("POST", "/api/v1/seeds/createUser", {
-        role: "ta",
-        courseId: student.course.id,
-      })
-        .then((res) => res.body)
-        .as("ta");
+    loginTA({
+      courseId: "student.course.id",
     });
-
-    cy.get("@queue").then((queue) => {
-      cy.get("@ta").then((ta) => {
-        cy.visit(`/api/v1/login/dev?userId=${ta.user.id}`);
-
-        // Check the TA into the queue
-        cy.request(
-          "POST",
-          `/api/v1/courses/${queue.course.id}/ta_location/${queue.room}`
-        );
-      });
+    checkInTA({
+      ta: "ta",
+      queue: "queue",
     });
-
-    // Login the student
-    cy.get("@student").then((student) => {
-      cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
+    createQuestion({
+      queueId: "queue.id",
+      userId: "student.user.id",
+      data: {
+        text: "How do I use the design recipe?",
+      },
     });
-
-    // Visit the queue page and create a question
-    cy.get("@queue").then((queue) =>
-      cy.visit(`/course/${queue.courseId}/queue/${queue.id}`).then(() => {
-        // Click "Join Queue"
-        cy.get("body").should("contain", "Join Queue");
-        cy.get('[data-cy="join-queue-button"]').click();
-
-        // Fill out the question form
-        cy.get("body").should("contain", "Concept");
-        cy.get("label").contains("Concept").click({
-          force: true,
-        });
-        cy.get("[data-cy='questionText']").type(
-          "How do I use the design recipe?"
-        );
-
-        // Click Submit
-        cy.get("[data-cy='finishQuestion']").click();
-      })
-    );
 
     //TA opens the student's question
     cy.get("@queue").then((queue) => {
@@ -74,27 +39,21 @@ describe("can't be found", () => {
     });
   });
 
-  it("Can't find student and student leaves the queue", () => {
-    cy.get("@queue").then((queue) => {
-      cy.get("@ta").then((ta) => {
-        cy.get("body").should("contain", "Help Next");
-        cy.get("button").contains("Help Next").click();
+  it("Can't find student and student leaves the queue", function () {
+    cy.get("body").should("contain", "Help Next");
+    cy.get("button").contains("Help Next").click();
 
-        // Click Can't Find
-        cy.get("body").should("contain", "Can't Find");
-        cy.get("button").contains("Can't Find").click();
+    // Click Can't Find
+    cy.get("body").should("contain", "Can't Find");
+    cy.get("button").contains("Can't Find").click();
 
-        cy.get("body").should("contain", "Yes");
-        cy.get("button").contains("Yes").click();
-      });
-    });
+    cy.get("body").should("contain", "Yes");
+    cy.get("button").contains("Yes").click();
 
-    // Login the student
-    cy.get("@student").then((student) => {
-      cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
-    });
-    cy.get("@queue").then((queue) =>
-      cy.visit(`course/${queue.courseId}/queue/${queue.id}`).then(() => {
+    cy.visit(`/api/v1/login/dev?userId=${this.student.user.id}`);
+
+    cy.visit(`course/${this.queue.courseId}/queue/${this.queue.id}`).then(
+      () => {
         cy.get("body").should(
           "contain",
           "A TA tried to help you, but couldn't reach you. Are you still in the queue? If you are, make sure you have Teams open, and rejoin the queue."
@@ -102,30 +61,24 @@ describe("can't be found", () => {
 
         //cy.get(".ant-modal").should("contain", "Leave Queue");
         cy.get("button").contains("Leave Queue").click();
-      })
+      }
     );
   });
 
-  it("Can't find student and student rejoins the queue", () => {
-    cy.get("@queue").then((queue) => {
-      cy.get("@ta").then((ta) => {
-        cy.get("body").should("contain", "Help Next");
-        cy.get("button").contains("Help Next").click();
-        // Click Can't Find
-        cy.get("body").should("contain", "Can't Find");
-        cy.get("button").contains("Can't Find").click();
+  it("Can't find student and student rejoins the queue", function () {
+    cy.get("body").should("contain", "Help Next");
+    cy.get("button").contains("Help Next").click();
+    // Click Can't Find
+    cy.get("body").should("contain", "Can't Find");
+    cy.get("button").contains("Can't Find").click();
 
-        cy.get("body").should("contain", "Yes");
-        cy.get("button").contains("Yes").click();
-      });
-    });
+    cy.get("body").should("contain", "Yes");
+    cy.get("button").contains("Yes").click();
 
-    // Login the student
-    cy.get("@student").then((student) => {
-      cy.visit(`/api/v1/login/dev?userId=${student.user.id}`);
-    });
-    cy.get("@queue").then((queue) =>
-      cy.visit(`course/${queue.courseId}/queue/${queue.id}`).then(() => {
+    cy.visit(`/api/v1/login/dev?userId=${this.student.user.id}`);
+
+    cy.visit(`course/${this.queue.courseId}/queue/${this.queue.id}`).then(
+      () => {
         cy.get("body").should(
           "contain",
           "A TA tried to help you, but couldn't reach you. Are you still in the queue? If you are, make sure you have Teams open, and rejoin the queue."
@@ -135,7 +88,7 @@ describe("can't be found", () => {
         cy.contains(
           "You are now in a priority queue, you will be helped soon. You were last helped by User."
         );
-      })
+      }
     );
   });
 });
