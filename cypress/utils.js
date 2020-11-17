61b49eb48455f551dfe5f148ae411a767e1520a7
Danish Farooq <>
2020-11-16 23:47:20 -0500

    adding documentation to the utils.js file

--- cypress/utils.js
+++ cypress/utils.js
@@ -1,3 +1,7 @@
+/**
+ *
+ * @param {string} id
+ */
 export const getId = (id) => {
   if (!id) {
     return cy.wrap();
@@ -26,6 +30,12 @@ const makeRequest = (request, identifier, { ...params }) => {
   }
 };
 
+/**
+ * Logs in a user with the given role into the given course and aliases it using the identifier.
+ * @param {string} role - One of "ta" or "student", represents a role a user has in our application.
+ * @param {stirng} courseId - The id of the course this user is being added to.
+ * @param {string} identifier -  The id that cypress will alias this logged-in user to.
+ */
 const loginUser = ({ role, courseId, identifier }) => {
   const req = (parsedId) => {
     // create the user
@@ -54,6 +64,11 @@ const loginUser = ({ role, courseId, identifier }) => {
   }
 };
 
+/**
+ * Logs in a TA into the course and binds the student to cypress with an alias of the identifier.
+ * @param {number} courseId - the course that this TA will be added to (defaults to null).
+ * @param {string} identifier - the id that cypress will alias this logged-in TA to (defaults to "ta").
+ */
 export const loginTA = ({ courseId = null, identifier = "ta" } = {}) =>
   loginUser({
     role: "ta",
@@ -61,6 +76,11 @@ export const loginTA = ({ courseId = null, identifier = "ta" } = {}) =>
     identifier,
   });
 
+/**
+ * Logs in a student into the course and binds the student to cypress with an alias of the identifier.
+ * @param {number} courseId - the course that this student will be added to (defaults to null).
+ * @param {string} identifier - the id that cypress will alias this logged-in student to. (defaults to "student").
+ */
 export const loginStudent = ({
   courseId = null,
   identifier = "student",
@@ -71,6 +91,13 @@ export const loginStudent = ({
     identifier,
   });
 
+/**
+ * Creates a new queue and binds it to cypress with an alias of the identifier.
+ * @param {number} courseId - the course that this queue will be added to (defaults to null).
+ * @param {number} room - the room where this queue will occur (defaults to "Online").
+ * @param {booelan} allowQuestions - whether or not you want to allow questions in the queue (defaults to true).
+ * @param {string} identifier - the id that cypress will alias this queue to (defaults to "queue").
+ */
 export const createQueue = ({
   courseId = null,
   room = "Online",
@@ -88,6 +115,13 @@ export const createQueue = ({
   });
 };
 
+/**
+ * Creates a question and binds it to cypress with an alias of the identifier.
+ * @param {number} queueId - the id of the queue this question is being added to.
+ * @param {number} userId - the id of the user specifying this question.
+ * @param {any} data - information about the question.
+ * @param {string} identifier - the id that cypress will alias this logged-in user to.
+ */
 export const createQuestion = ({ queueId, userId, data, identifier }) => {
   const req = ({ queueId, userId }) =>
     cy.request("POST", "/api/v1/seeds/createQuestion", {
@@ -103,6 +137,11 @@ export const createQuestion = ({ queueId, userId, data, identifier }) => {
   });
 };
 
+/**
+ * Checks the TA into the queue.
+ * @param {string} queue - the identifier alias of the queue for cypress.
+ * @param {string} userId - the identifier alias of the TA for cypress.
+ */
 export const checkInTA = ({ ta, queue }) => {
   cy.get(`@${ta}`).then((ta) => {
     cy.get(`@${queue}`).then((queue) => {
