30678ee53bbc11849987d357d5b7f05f94130d29
willstenzel <stenzel.will@gmail.com>
2020-11-19 13:51:12 -0700

    Fix test

--- cypress/integration/student/student_priority_queue.spec.js
+++ cypress/integration/student/student_priority_queue.spec.js
@@ -97,10 +97,7 @@ describe("Student Priority Queue", () => {
     it("TA removes student question from the queue and student rejoins", function () {
       // Click on the student's question
       cy.get("[data-cy='ta-queue-card']").should("be.visible").click();
-      // Click help
-      cy.get("[data-cy='help-student']").click();
-      // Click Remove from queue on the Question card
-      cy.contains("You are helping");
+      // Click Remove from queue from the sidebar
       cy.get("[data-cy='remove-from-queue']").first().click();
 
       cy.get("body").should("contain", "Yes");
@@ -113,7 +110,7 @@ describe("Student Priority Queue", () => {
         () => {
           cy.get("body").should(
             "contain",
-            "A TA tried to help you, but couldn't reach you. Are you still in the queue? If you are, make sure you have Teams open, and rejoin the queue."
+            "You've been removed from the queue by a TA. If you have any questions, please reach out to the TA. If you'd like to join back into the queue with your previous question, click Rejoin Queue, otherwise click Leave Queue."
           );
 
           cy.get("body").should("contain", "Rejoin Queue");
@@ -121,10 +118,7 @@ describe("Student Priority Queue", () => {
           cy.get("button").contains("Rejoin Queue").click();
 
           // Check that the student was sucessfully but back into the queue
-          cy.get("body").should(
-            "contain",
-            "You are now in a priority queue, you will be helped soon. You were last helped by User."
-          );
+          cy.get("body").should("contain", "How do I use the design recipe?");
         }
       );
     });
