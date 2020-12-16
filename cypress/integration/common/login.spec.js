103edb9dde47df5b199049e6c67525148ed8683f
NEUDitao <edward.d.li@gmail.com>
2020-12-16 01:01:19 -0500

    logout cypress test

--- cypress/integration/common/login.spec.js
+++ cypress/integration/common/login.spec.js
@@ -1,6 +1,17 @@
+const { createAndLoginTA } = require("../../utils");
+
 describe("Login", () => {
   it("redirect to login if not logged in", () => {
     cy.visit("/");
     cy.location("pathname").should("eq", "/login");
+  });
+
+  it("logout works", () => {
+    createAndLoginTA();
+    cy.visit(`/`);
+    cy.get(".ant-modal-close-x").click();
+    cy.get(".ant-avatar").click();
+    cy.get(":nth-child(4) > :nth-child(2) > a").click();
+    cy.get("body").should("contain", "You are currently not logged in");
   });
 });
