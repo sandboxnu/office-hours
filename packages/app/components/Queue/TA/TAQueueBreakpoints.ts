845ec9236101e3546e2d8048f29d397218d15be0
Da-Jin Chu <dajinchu@gmail.com>
2020-12-19 00:06:21 -0500

    place in line as badge

--- packages/app/components/Queue/TA/TAQueueBreakpoints.ts
+++ packages/app/components/Queue/TA/TAQueueBreakpoints.ts
@@ -3,5 +3,3 @@
  */
 // The min screen width at which the list and detail become side-by-side
 export const SPLIT_DETAIL_BKPT = 900;
-// The min screen width at which things can go from compact to comfortable
-export const COMPACT_BKPT = 1100;
