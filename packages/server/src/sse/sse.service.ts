ce9f95f8c2b49f99ad3b0500d04055b1d984698b
Da-Jin Chu <dajinchu@gmail.com>
2020-12-20 05:02:19 -0500

    redis sse

--- packages/server/src/sse/sse.service.ts
+++ packages/server/src/sse/sse.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { each } from 'async';
 import { serialize } from 'class-transformer';
 import * as apm from 'elastic-apm-node';
 import { Response } from 'express';
@@ -7,6 +8,11 @@ import { RedisService } from 'nestjs-redis';
 export interface Client<T> {
   metadata: T;
   res: Response;
+}
+
+interface Connection {
+  res: Response;
+  cleanup: () => Promise<void>;
 }
 
 interface RedisClientInfo<T> {
@@ -19,28 +25,33 @@ interface RedisClientInfo<T> {
  * Low level abstraction for sending SSE to "rooms" of clients.
  * Probably don't use this directly, and wrap it in a service specific to that event source
  *
- * This handles when there's multiple backend instances by assigning unique client ids.
+ * This handles when there's multiple backend instances by assigning unique client ids to each connection.
  * When one instance wants to send to a client, it publishes to a Redis channel for the client.
  * All instances listen to Redis, and if they are the one managing that client, they send the msg.
  *
  * Rooms with client metadata are also maintained in Redis key/value store.
  */
 @Injectable()
-export class SSEService<T> {
+export class SSEService<T> implements OnModuleDestroy {
   // Clients connected to this instance of the backend
-  private clientsConnectedDirectly: Record<number, Response> = {};
+  private directConnnections: Record<string, Connection> = {};
 
   constructor(private readonly redisService: RedisService) {
     const redisSub = this.redisService.getClient('sub');
 
     // If channel is managed by this instance, send the message to the Response object.
     redisSub.on('message', (channel, message) => {
-      console.log('MESSAGE');
       const id = /sse::client-(\d+)/.exec(channel);
-      console.log(id, Object.keys(this.clientsConnectedDirectly));
-      if (id && id[1] in this.clientsConnectedDirectly) {
-        this.clientsConnectedDirectly[id[1]].write(`data: ${message}\n\n`);
+      if (id && id[1] in this.directConnnections) {
+        this.directConnnections[id[1]].res.write(`data: ${message}\n\n`);
       }
+    });
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    // Cleanup all direct connections by removing them from the rooms in redis.
+    await each(Object.values(this.directConnnections), async (conn) => {
+      await conn.cleanup();
     });
   }
 
@@ -56,8 +67,7 @@ export class SSEService<T> {
     const redisSub = this.redisService.getClient('sub');
     const redis = this.redisService.getClient('db');
     // Keep track of responses so we can send sse through them
-    const clientId = await redis.incr('client::id');
-    this.clientsConnectedDirectly[clientId] = client.res;
+    const clientId = await redis.incr('sse::client::id');
     // Subscribe to the redis channel for this client
     await redisSub.subscribe(this.idToChannel(clientId));
 
@@ -68,10 +78,19 @@ export class SSEService<T> {
     } as RedisClientInfo<T>);
     await redis.sadd(room, clientInfo);
 
+    // Keep track of response object in direct connections
+    this.directConnnections[clientId] = {
+      res: client.res,
+      cleanup: async () => {
+        // Remove from the redis room
+        await redis.srem(room, clientInfo);
+      },
+    };
+
     // Remove dead connections!
     client.res.socket.on('end', async () => {
-      delete this.clientsConnectedDirectly[clientId];
-      await redis.srem(room, clientInfo);
+      await this.directConnnections[clientId].cleanup();
+      delete this.directConnnections[clientId];
     });
   }
 
@@ -84,14 +103,14 @@ export class SSEService<T> {
     const redis = this.redisService.getClient('db');
     const roomInfo = await redis.smembers(room);
     if (room) {
-      const clients: RedisClientInfo<T>[] = roomInfo.map(s => JSON.parse(s));
+      const clients: RedisClientInfo<T>[] = roomInfo.map((s) => JSON.parse(s));
       console.log(`sending sse to ${clients.length} clients in ${room}`);
       console.time(`sending sse time: `);
       apm.startTransaction('sse');
-      for (const { clientId, metadata } of clients) {
+      await each(clients, async ({ clientId, metadata }) => {
         const toSend = serialize(await payload(metadata));
         await redisPub.publish(this.idToChannel(clientId), toSend);
-      }
+      });
       apm.endTransaction();
       console.timeEnd(`sending sse time: `);
     }
