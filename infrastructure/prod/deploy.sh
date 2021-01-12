7d4b717760f0deaf74f8537fcc383c22cb383cea
Da-Jin Chu <dajinchu@gmail.com>
2021-01-12 02:31:00 -0500

    deploy script

---
+++ infrastructure/prod/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Runs deployment on this machine. Do NOT run this locally.
+set -ex
+
+cd /var/www/source
+git fetch
+git reset --hard origin/dist
+yarn
+yarn typeorm migration:run
+env HOME=/var/www pm2 startOrReload infrastructure/prod/ecosystem.config.js
