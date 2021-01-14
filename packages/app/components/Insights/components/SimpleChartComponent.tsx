946399c704b82f45d789a67c9be6ab0b0e113607
willstenzel <stenzel.will@gmail.com>
2021-01-14 15:32:20 -0500

    Add ant design charts package

--- packages/app/components/Insights/components/SimpleChartComponent.tsx
+++ packages/app/components/Insights/components/SimpleChartComponent.tsx
@@ -1,21 +1,39 @@
-// import { Bar } from "@ant-design/charts";
+import dynamic from "next/dynamic";
+
 import { SimpleChartOutputType } from "@koh/common";
 import React, { ReactElement } from "react";
+
+const Bar = dynamic(() => import("@ant-design/charts/lib/bar"));
 
 interface SimpleChartTypes {
   output: SimpleChartOutputType;
 }
 
 function SimpleChartComponent({ output }: SimpleChartTypes): ReactElement {
+  const data = [
+    { year: "1951 year", value: 38 },
+    { year: "1952 year", value: 52 },
+    { year: "1956 year", value: 61 },
+    { year: "1957 year", value: 145 },
+    { year: "1958 year", value: 48 },
+  ];
   const config = {
-    data: output.data,
-    xField: output.xAxisName,
-    yField: output.yAxisName,
-    seriesField: output.xAxisName,
+    data: data,
+    xField: "value",
+    yField: "year",
+    seriesField: "year",
+    legend: { position: "top-left" },
   };
-  // TODO: Figure out why this is erroring
-  // return <Bar {...config} />;
-  return null;
+
+  // const config = {
+  //   data: output.data,
+  //   xField: output.xAxisName,
+  //   yField: output.yAxisName,
+  //   seriesField: output.xAxisName,
+  // };
+
+  // @ts-ignore
+  return <Bar {...config} />;
 }
 
 export default SimpleChartComponent;
