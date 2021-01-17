0a85bdced91fc148e72e07f9c3818b9198387902
Stanley Liu <liu.sta@northeastern.edu>
2021-01-17 16:24:46 -0500

    Add console log for Khoury Login body

--- packages/server/src/login/login.controller.ts
+++ packages/server/src/login/login.controller.ts
@@ -34,6 +34,8 @@ export class LoginController {
     @Req() req: Request,
     @Body() body: KhouryDataParams,
   ): Promise<KhouryRedirectResponse> {
+    console.log('Khoury Login Body', JSON.stringify(body));
+
     if (process.env.NODE_ENV === 'production') {
       // Check that request has come from Khoury
       const parsedRequest = httpSignature.parseRequest(req);
