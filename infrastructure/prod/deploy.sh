aa0c498b6be87a9a669737e1d7937188dbb936be
Da-Jin Chu <dajinchu@gmail.com>
2021-01-24 20:04:07 -0500

    chgrp correctly

--- infrastructure/prod/deploy.sh
+++ infrastructure/prod/deploy.sh
@@ -8,4 +8,4 @@ git reset --hard origin/dist
 yarn
 yarn typeorm migration:run
 env HOME=/var/www pm2 startOrReload infrastructure/prod/ecosystem.config.js
-chgrp -R officehours /var/www/source/.pm2
+chgrp -R officehours /var/www/.pm2
