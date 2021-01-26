739edf6112721937b1eab77b12142998e68817fe
ethan-leba <ethanleba5@gmail.com>
2021-01-26 17:36:43 -0500

    Fix minor spelling error (sucsess -> success)

--- cypress/integration/common/notification_settings.spec.js
+++ cypress/integration/common/notification_settings.spec.js
@@ -5,12 +5,12 @@ describe("Notification settings", () => {
     createAndLoginStudent();
   });
 
-  it("can sucsessfully enable web notifications", () => {
+  it("can successfully enable web notifications", () => {
     // Click the profile icon
     // Click to enable web notifications
   });
 
-  it("can sucsessfully enable text notifications", () => {
+  it("can successfully enable text notifications", () => {
     // Click the profile icon
     // Click to enable text notifications
     // Input a phone number
