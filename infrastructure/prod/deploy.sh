6ddaef1644da0e9d5252a9fb14bd8671caa97fe2
Da-Jin Chu <dajinchu@gmail.com>
2021-02-07 12:53:11 -0500

    no draft release

--- infrastructure/prod/deploy.sh
+++ infrastructure/prod/deploy.sh
@@ -2,10 +2,8 @@
 # Runs deployment on this machine. Do NOT run this locally.
 set -ex
 
-cd /var/www/source
-git fetch
-git reset --hard origin/dist
-yarn
+cd /var/www/source-v2
+wget https://github.com/sandboxnu/office-hours/releases/latest/download/release.zip
+unzip -o release.zip
 yarn typeorm migration:run
 env HOME=/var/www pm2 startOrReload infrastructure/prod/ecosystem.config.js
-chgrp -R officehours /var/www/.pm2
