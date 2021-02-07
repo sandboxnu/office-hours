85df9f1a89b66188ed357ca5c9516804140472f8
willstenzel <stenzel.will@gmail.com>
2021-02-07 16:02:28 -0500

    Fix issue with checking for open oh at weird time

--- packages/server/src/queue/queue.entity.spec.ts
+++ packages/server/src/queue/queue.entity.spec.ts
@@ -1,7 +1,11 @@
 import { ClosedQuestionStatus, OpenQuestionStatus } from '@koh/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Connection } from 'typeorm';
-import { QuestionFactory, QueueFactory } from '../../test/util/factories';
+import {
+  OfficeHourFactory,
+  QuestionFactory,
+  QueueFactory,
+} from '../../test/util/factories';
 import { TestTypeOrmModule } from '../../test/util/testUtils';
 import { QueueModel } from './queue.entity';
 
@@ -56,5 +60,44 @@ describe('queue entity', () => {
     await queue.addQueueSize();
 
     expect(queue.queueSize).toBe(2);
+  });
+
+  it('areTheirOfficeHoursRightNow works when oh time spans across midnight', async () => {
+    const oh = await OfficeHourFactory.create({
+      startTime: new Date('2020-12-13T22:53:57.254Z'),
+      endTime: new Date('2020-12-14T00:08:57.254Z'),
+    });
+
+    const queue = await QueueFactory.create({
+      officeHours: [oh],
+    });
+
+    // Should be false when time is 10 min before office hours
+    expect(
+      await queue.areThereOfficeHoursRightNow(
+        new Date('2020-12-13T22:40:00.254Z'),
+      ),
+    ).toBeFalsy();
+
+    // Should return true when time is within 10 min of the start of office hours
+    expect(
+      await queue.areThereOfficeHoursRightNow(
+        new Date('2020-12-13T22:50:00.254Z'),
+      ),
+    ).toBeTruthy();
+
+    // Should be true when time is during office hours and before midnight UTC
+    expect(
+      await queue.areThereOfficeHoursRightNow(
+        new Date('2020-12-13T22:58:57.254Z'),
+      ),
+    ).toBeTruthy();
+
+    // Should be true when time is during office hours and after midnight UTC
+    expect(
+      await queue.areThereOfficeHoursRightNow(
+        new Date('2020-12-14T00:04:00.254Z'),
+      ),
+    ).toBeTruthy();
   });
 });
