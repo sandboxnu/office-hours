e64d639732e1653708505ff127274f475842708d
NEUDitao <edward.d.li@gmail.com>
2021-02-22 01:28:29 -0500

    added chgrp to deploy script

--- infrastructure/prod/deploy.sh
+++ infrastructure/prod/deploy.sh
@@ -9,3 +9,4 @@ yarn
 yarn typeorm migration:run
 env HOME=/var/www pm2 startOrReload infrastructure/prod/ecosystem.config.js
 chgrp -R officehours /var/www/.pm2
+chgrp -R officehours /var/www/source
