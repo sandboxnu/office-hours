62fd04c15a4bba7445802bcdb58619c7906ee837
willstenzel <stenzel.will@gmail.com>
2021-03-17 13:09:01 -0400

    Anonomize phone numbers

--- infrastructure/prod/anonymize_sql.py
+++ infrastructure/prod/anonymize_sql.py
@@ -5,16 +5,28 @@ os.system('pg_dump prod > prod.sql')
 prod = open('prod.sql', 'r')
 anonomized_prod = open('anonomized_prod.sql', 'w')
 
-anonymizing = False
+anonymizing_user = False
+anonymizing_phone_numbers = False
 
 lines = prod.readlines()
 
 for line in lines:
   newLine = line
 
-  if "\.\n" == line:
-    anonymizing = False
+
+## Anonomize phone numbers
+if "\.\n" == line:
+  anonymizing_phone_numbers = False
   
+if "COPY public.phone_notif_model" in line:
+  anonymizing_phone_numbers = True
+
+
+## Anonomize user data
+if "\.\n" == line:
+  anonymizing_user = False
+
+if anonymizing_user:
   elements = line.split()
   id = elements[0] # user id
   elements[1] = id + '@northeastern.edu' #email
@@ -22,11 +34,12 @@ for line in lines:
   elements[5] = 'user' + id # first name
   elements[6] = 'person' + id # last name
 
-    newLine = " ".join(elements) + '\n'
+  newLine = " ".join(elements) + '\n'
 
-  if "COPY public.user_model" in line:
-    anonymizing = True
+if "COPY public.user_model" in line:
+  anonymizing_user = True
 
+if not anonymizing_phone_numbers:
   anonomized_prod.write(newLine)
 
 prod.close()
