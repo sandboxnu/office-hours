93acec25e285058a641a8929cdea1779fb4fa136
Danish Farooq <>
2021-03-17 19:16:49 -0400

    PEP8 formatting from pycharm, comments, and typo fixes

--- infrastructure/prod/anonymize_sql.py
+++ infrastructure/prod/anonymize_sql.py
@@ -1,9 +1,11 @@
 import os
 
+# Create a SQL dump with prod data
 os.system('pg_dump prod > prod.sql')
 
-prod = open('prod.sql', 'r')
-anonomized_prod = open('anonomized_prod.sql', 'w')
+# Read the data and write a new file with anonymized user data and phone numbers
+prod = open('prod.sql')
+anonymized_prod = open('anonymized_prod.sql', 'w')
 
 anonymizing_user = False
 anonymizing_phone_numbers = False
@@ -11,38 +13,37 @@ anonymizing_phone_numbers = False
 lines = prod.readlines()
 
 for line in lines:
-  newLine = line
+    newLine = line
 
+    # Anonymize phone numbers
+    if "\.\n" == line:
+        anonymizing_phone_numbers = False
 
-## Anonomize phone numbers
-if "\.\n" == line:
-  anonymizing_phone_numbers = False
-  
-if "COPY public.phone_notif_model" in line:
-  anonymizing_phone_numbers = True
+    if "COPY public.phone_notif_model" in line:
+        anonymizing_phone_numbers = True
 
+    # Anonymize user data
+    if "\.\n" == line:
+        anonymizing_user = False
 
-## Anonomize user data
-if "\.\n" == line:
-  anonymizing_user = False
+    if anonymizing_user:
+        elements = line.split()
+        id = elements[0]  # user id
+        elements[1] = id + '@northeastern.edu'  # email
+        elements[2] = ''  # photoURL
+        elements[5] = 'user' + id  # first name
+        elements[6] = 'person' + id  # last name
 
-if anonymizing_user:
-  elements = line.split()
-  id = elements[0] # user id
-  elements[1] = id + '@northeastern.edu' #email
-  elements[2] = '' # photoURL
-  elements[5] = 'user' + id # first name
-  elements[6] = 'person' + id # last name
+        newLine = " ".join(elements) + '\n'
 
-  newLine = " ".join(elements) + '\n'
+    if "COPY public.user_model" in line:
+        anonymizing_user = True
 
-if "COPY public.user_model" in line:
-  anonymizing_user = True
-
-if not anonymizing_phone_numbers:
-  anonomized_prod.write(newLine)
+    if not anonymizing_phone_numbers:
+        anonymized_prod.write(newLine)
 
 prod.close()
-anonomized_prod.close()
+anonymized_prod.close()
 
+# Remove the original file with
 os.remove('prod.sql')
