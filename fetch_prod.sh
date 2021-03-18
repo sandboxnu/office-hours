db5f53d944ba1b335022fb0e2314373921cd075e
Stanley Liu <liu.sta@northeastern.edu>
2021-03-17 20:06:19 -0400

    add office hours role

--- fetch_prod.sh
+++ fetch_prod.sh
@@ -6,4 +6,4 @@ user=$1
 
 cat ./infrastructure/prod/anonymize.sh | ssh -J $user@login.ccs.neu.edu $user@amato.ccs.neu.edu
 sleep 5
-scp $user@amato.ccs.neu.edu:/var/www/source/infrastructure/prod/anonomized_prod.sql ./
+scp $user@amato.ccs.neu.edu:/var/www/source/infrastructure/prod/anonymized_prod.sql ./
