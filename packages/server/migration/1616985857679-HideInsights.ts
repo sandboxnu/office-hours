3c11681afb2d7c521fd389bf90568689bd0fddaf
willstenzel <stenzel.will@gmail.com>
2021-03-28 22:45:57 -0400

    I should stop coding for today

--- packages/server/migration/1616985857679-HideInsights.ts
+++ packages/server/migration/1616985857679-HideInsights.ts
@@ -1,22 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class HideInsightsAndAddDefault1616979799180
-  implements MigrationInterface {
-  name = 'HideInsightsAndAddDefault1616979799180';
+export class HideInsights1616985857679 implements MigrationInterface {
+  name = 'HideInsights1616985857679';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "user_model" RENAME COLUMN "insights" TO "hideInsights"`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "user_model" ALTER COLUMN "hideInsights" SET DEFAULT '[]'`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "user_model" ALTER COLUMN "hideInsights" DROP DEFAULT`,
-    );
     await queryRunner.query(
       `ALTER TABLE "user_model" RENAME COLUMN "hideInsights" TO "insights"`,
     );
