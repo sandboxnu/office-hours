a764c3d88763ce09cc37c610f4928286760bf4fb
Stanley Liu <liu.sta@northeastern.edu>
2021-04-25 21:22:44 -0400

    fix guard import

--- packages/server/src/semester/semester.controller.ts
+++ packages/server/src/semester/semester.controller.ts
@@ -1,6 +1,6 @@
 import { SemesterPartial } from '@koh/common';
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'login/jwt-auth.guard';
+import { JwtAuthGuard } from 'guards/jwt-auth.guard';
 import { SemesterModel } from './semester.entity';
 
 @Controller('semesters')
