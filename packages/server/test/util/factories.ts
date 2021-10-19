b3d7b28df15e1b0ae925dc975c49efe4b790b808
Vera <vkong6019@gmail.com>
2021-10-19 19:28:44 -0400

    oops

--- packages/server/test/util/factories.ts
+++ packages/server/test/util/factories.ts
@@ -45,7 +45,6 @@ export const CourseFactory = new Factory(CourseModel)
   .attr('name', 'CS 2500')
   .attr('icalURL', 'http://hi.com')
   .attr('enabled', true)
-  .attr('coordinator_email', 'yayeetyeet@test.com')
   .assocOne('semester', SemesterFactory)
   .assocMany('officeHours', OfficeHourFactory, 0);
 
