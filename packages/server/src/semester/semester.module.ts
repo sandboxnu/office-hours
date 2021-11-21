4520b1abfa3017033a1541c6dbceeb54f5a544e5
tiingweii-shii <shi.ting@husky.neu.edu>
2021-11-21 17:05:44 -0500

    args validation

--- packages/server/src/semester/semester.module.ts
+++ packages/server/src/semester/semester.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SemesterController } from './semester.controller';
+import { SemesterService } from './semester.service';
 import { SetSemesterCommand } from './setCourse.command';
 
 @Module({
   controllers: [SemesterController],
-  providers: [SetSemesterCommand],
+  providers: [SetSemesterCommand, SemesterService],
 })
 export class SemesterModule {}
