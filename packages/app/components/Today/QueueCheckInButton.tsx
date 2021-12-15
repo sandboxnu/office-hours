9a434e6b53a589358897d25d1fb20fd2d91a37e9
Neel Bhalla <ashdawngary@gmail.com>
2021-12-15 13:50:05 -0500

    queue checkin done

--- packages/app/components/Today/QueueCheckInButton.tsx
+++ packages/app/components/Today/QueueCheckInButton.tsx
@@ -1,41 +1,43 @@
 import { API } from "@koh/api-client";
 import { QueuePartial, Role } from "@koh/common";
-import { Form, Input, message, Modal, Radio } from "antd";
+import { Form, message, Modal, Select } from "antd";
 import { useRouter } from "next/router";
-import { Row } from "antd";
 import React, { ReactElement, useState } from "react";
-import styled from "styled-components";
 import { useCourse } from "../../hooks/useCourse";
 import { useProfile } from "../../hooks/useProfile";
 import { useRoleInCourse } from "../../hooks/useRoleInCourse";
 import TACheckinButton, { CheckinButton } from "./TACheckinButton";
 
-const ModalRadio = styled(Radio)`
-  display: block;
-  height: 30px;
-`;
-
 export default function TodayPageCheckinButton(): ReactElement {
   const profile = useProfile();
   const [modalVisible, setModalVisible] = useState(false);
-  const [queueToCheckInto, setQueueToCheckInto] = useState(0);
+  const [queueToCheckInto, setQueueToCheckInto] = useState(-1);
   const router = useRouter();
   const { cid } = router.query;
   const { course } = useCourse(Number(cid));
   const role = useRoleInCourse(Number(cid));
   const [form] = Form.useForm();
+  const { Option } = Select;
   const queueCheckedIn = course?.queues.find((queue) =>
     queue.staffList.find((staff) => staff.id === profile?.id)
   );
+
+  function onQueueUpdate(queueIx: number) {
+    setQueueToCheckInto(queueIx);
+  }
 
   return (
     <>
       {modalVisible && (
         <Modal
-          title="Check-In To Office Hours"
+          title="Check into an existing queue"
           visible={modalVisible}
-          onCancel={() => setModalVisible(false)}
+          onCancel={() => {
+            setModalVisible(false);
+            setQueueToCheckInto(-1);
+          }}
           okText="Check In"
+          okButtonProps={{ disabled: queueToCheckInto < 0 }}
           onOk={async () => {
             const value = await form.validateFields();
             let redirectID: QueuePartial;
@@ -60,45 +62,25 @@ export default function TodayPageCheckinButton(): ReactElement {
             }
           }}
         >
-          <h3>Which queue would you like to check into?</h3>
-          <Radio.Group
-            value={queueToCheckInto}
-            onChange={(e) => setQueueToCheckInto(e.target.value)}
+          <h3>Begin searching for a room</h3>
+          <Select
+            showSearch
+            style={{ width: 200 }}
+            placeholder="Select a course"
+            optionFilterProp="children"
+            onChange={onQueueUpdate}
+            filterOption={(input, option) => {
+              return (
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              );
+            }}
           >
             {course?.queues
               .filter((q) => (role === Role.TA ? !q.isProfessorQueue : true))
               .map((q, i) => (
-                <ModalRadio key={q.id} value={i}>
-                  {q.room}
-                </ModalRadio>
+                <Option key={i} value={i}>{`${q.room}`}</Option>
               ))}
-            <Row style={{ flexWrap: "nowrap" }}>
-              <ModalRadio value={-1}>Other...</ModalRadio>
-              {queueToCheckInto === -1 ? (
-                <Form form={form}>
-                  <Form.Item
-                    name="officeHourName"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please give this room a name.",
-                      },
-                    ]}
-                  >
-                    <Input
-                      defaultValue={
-                        role === Role.TA
-                          ? ""
-                          : `Professor ${profile.lastName}'s Office Hours`
-                      }
-                      placeholder={"Please name the room."}
-                      style={{ width: 350 }}
-                    />
-                  </Form.Item>
-                </Form>
-              ) : null}
-            </Row>
-          </Radio.Group>
+          </Select>
         </Modal>
       )}
       {!queueCheckedIn && role !== Role.STUDENT && (
