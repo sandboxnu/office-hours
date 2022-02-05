f20f2eabf700533f3afde66981324c4ac38403ad
sumitde22 <39414740+sumitde22@users.noreply.github.com>
2022-02-05 17:03:00 -0500

    fix integration test

--- cypress/integration/common/no_courses_logout.spec.js
+++ cypress/integration/common/no_courses_logout.spec.js
@@ -1,15 +1,19 @@
+const { createAndLoginStudent } = require("../../utils")
+
 describe("Logging out for no course user", () => {
-    it("test", () => {
-        cy.visit("/dev")
-        cy.contains("Seed Data").click()
-        cy.contains("No Course User").click()
-        cy.get("body").should("contain", "None of your courses are using the Khoury Office Hours App.");
-        cy.get("[data-cy='logout-button']").should(
-            "have.attr",
-            "href",
-            "/api/v1/logout"
-          );
-        cy.visit("/api/v1/logout");
-        cy.get("body").should("contain", "You are currently not logged in");
+    it("actual test", () => {
+        createAndLoginStudent()
+        cy.get("@student").then((student) => {
+            cy.request('DELETE', `/api/v1/courses/${student.course.id}/withdraw_course`)
+            cy.visit('/')
+            cy.get("body").should("contain", "None of your courses are using the Khoury Office Hours App.");
+            cy.get("[data-cy='logout-button']").should(
+                "have.attr",
+                "href",
+                "/api/v1/logout"
+              );
+              cy.visit("/api/v1/logout");
+              cy.get("body").should("contain", "You are currently not logged in");
+        });
     })
 })
