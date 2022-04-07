344ae7dd43fe4c12d6d72878aea3aee5ac7254dc
Vera Kong <vkong6019@gmail.com>
2022-04-07 18:53:42 -0400

    Remove queue clean on checkout (#865)
    * ERADICATION
    
    * fix test
    
    * update cypress tests
    
    * rm unused field
    
    * yarn wtf

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -11,7 +11,7 @@ import {
   IsObject,
   IsOptional,
   IsString,
-  ValidateIf,
+  ValidateIf
 } from "class-validator";
 import "reflect-metadata";
 
@@ -120,7 +120,7 @@ export type UserCourse = {
 export enum Role {
   STUDENT = "student",
   TA = "ta",
-  PROFESSOR = "professor",
+  PROFESSOR = "professor"
 }
 /**
  * A Queue that students can join with thier tickets.
@@ -228,14 +228,14 @@ export enum QuestionType {
   Testing = "Testing",
   Bug = "Bug",
   Setup = "Setup",
-  Other = "Other",
+  Other = "Other"
 }
 
 export enum OpenQuestionStatus {
   Drafting = "Drafting",
   Queued = "Queued",
   Helping = "Helping",
-  PriorityQueued = "PriorityQueued",
+  PriorityQueued = "PriorityQueued"
 }
 
 /**
@@ -244,19 +244,19 @@ export enum OpenQuestionStatus {
 export enum LimboQuestionStatus {
   CantFind = "CantFind", // represents when a student can't be found by a TA
   ReQueueing = "ReQueueing", // represents when a TA wants to get back to a student later and give them the option to be put into the priority queue
-  TADeleted = "TADeleted", // When a TA deletes a question for a multitude of reasons
+  TADeleted = "TADeleted" // When a TA deletes a question for a multitude of reasons
 }
 
 export enum ClosedQuestionStatus {
   Resolved = "Resolved",
   DeletedDraft = "DeletedDraft",
   ConfirmedDeleted = "ConfirmedDeleted",
-  Stale = "Stale",
+  Stale = "Stale"
 }
 
 export const StatusInQueue = [
   OpenQuestionStatus.Drafting,
-  OpenQuestionStatus.Queued,
+  OpenQuestionStatus.Queued
 ];
 
 export const StatusInPriorityQueue = [OpenQuestionStatus.PriorityQueued];
@@ -267,7 +267,7 @@ export const StatusSentToCreator = [
   OpenQuestionStatus.Helping,
   LimboQuestionStatus.ReQueueing,
   LimboQuestionStatus.CantFind,
-  LimboQuestionStatus.TADeleted,
+  LimboQuestionStatus.TADeleted
 ];
 
 // Ticket Status - Represents a given status of as student's ticket
@@ -276,7 +276,7 @@ export type QuestionStatus = keyof typeof QuestionStatusKeys;
 export const QuestionStatusKeys = {
   ...OpenQuestionStatus,
   ...ClosedQuestionStatus,
-  ...LimboQuestionStatus,
+  ...LimboQuestionStatus
 };
 
 export class QuestionGroup {
@@ -401,7 +401,7 @@ export class UpdateProfileParams {
   @IsOptional()
   phoneNotifsEnabled?: boolean;
 
-  @ValidateIf((o) => o.phoneNotifsEnabled)
+  @ValidateIf(o => o.phoneNotifsEnabled)
   @IsString()
   @IsNotEmpty()
   phoneNumber?: string;
@@ -565,10 +565,6 @@ export type QueueNotePayloadType = {
 export class TACheckoutResponse {
   // The ID of the queue we checked out of
   queueId!: number;
-  canClearQueue!: boolean;
-
-  @Type(() => Date)
-  nextOfficeHourTime?: Date;
 }
 
 export class UpdateQueueParams {
@@ -608,7 +604,7 @@ export class TACheckinPair {
 }
 
 export enum AlertType {
-  REPHRASE_QUESTION = "rephraseQuestion",
+  REPHRASE_QUESTION = "rephraseQuestion"
 }
 
 export class AlertPayload {}
@@ -746,7 +742,7 @@ export interface InsightObject {
 export enum InsightComponent {
   SimpleDisplay = "SimpleDisplay",
   BarChart = "BarChart",
-  SimpleTable = "SimpleTable",
+  SimpleTable = "SimpleTable"
 }
 
 export type PossibleOutputTypes =
@@ -783,7 +779,7 @@ export const ERROR_MESSAGES = {
   courseController: {
     checkIn: {
       cannotCheckIntoMultipleQueues:
-        "Cannot check into multiple queues at the same time",
+        "Cannot check into multiple queues at the same time"
     },
     courseAlreadyRegistered: "One or more of the courses is already registered",
     courseNotFound: "The course was not found",
@@ -810,14 +806,14 @@ export const ERROR_MESSAGES = {
     updateProfLastRegistered:
       "Unable to update professor's last registered semester",
     invalidApplyURL:
-      "You are unauthorized to submit an application. Please email help@khouryofficehours.com for the correct URL.",
+      "You are unauthorized to submit an application. Please email help@khouryofficehours.com for the correct URL."
   },
   questionController: {
     createQuestion: {
       invalidQueue: "Posted to an invalid queue",
       noNewQuestions: "Queue not allowing new questions",
       closedQueue: "Queue is closed",
-      oneQuestionAtATime: "You can't create more than one question at a time.",
+      oneQuestionAtATime: "You can't create more than one question at a time."
     },
     updateQuestion: {
       fsmViolation: (
@@ -830,32 +826,32 @@ export const ERROR_MESSAGES = {
       otherTAHelping: "Another TA is currently helping with this question",
       otherTAResolved: "Another TA has already resolved this question",
       taHelpingOther: "TA is already helping someone else",
-      loginUserCantEdit: "Logged-in user does not have edit access",
+      loginUserCantEdit: "Logged-in user does not have edit access"
     },
     groupQuestions: {
-      notGroupable: "One or more of the questions is not groupable",
+      notGroupable: "One or more of the questions is not groupable"
     },
     saveQError: "Unable to save a question",
     notFound: "Question not found",
-    unableToNotifyUser: "Unable to notify user",
+    unableToNotifyUser: "Unable to notify user"
   },
   loginController: {
     receiveDataFromKhoury: "Invalid request signature",
     invalidPayload: "The decoded JWT payload is invalid",
     invalidTempJWTToken: "Error occurred while signing a JWT token",
     addUserFromKhoury:
-      "Error occurred while translating account from Khoury to Office Hours",
+      "Error occurred while translating account from Khoury to Office Hours"
   },
   notificationController: {
-    messageNotFromTwilio: "Message not from Twilio",
+    messageNotFromTwilio: "Message not from Twilio"
   },
   notificationService: {
-    registerPhone: "phone number invalid",
+    registerPhone: "phone number invalid"
   },
   questionRoleGuard: {
     questionNotFound: "Question not found",
     queueOfQuestionNotFound: "Cannot find queue of question",
-    queueDoesNotExist: "This queue does not exist!",
+    queueDoesNotExist: "This queue does not exist!"
   },
   queueController: {
     getQueue: "An error occurred while trying to retrieve a Queue",
@@ -863,38 +859,38 @@ export const ERROR_MESSAGES = {
     saveQueue: "Unable to save queue",
     cleanQueue: "Unable to clean queue",
     cannotCloseQueue: "Unable to close professor queue as a TA",
-    missingStaffList: "Stafflist relation not present on Queue",
+    missingStaffList: "Stafflist relation not present on Queue"
   },
   queueRoleGuard: {
-    queueNotFound: "Queue not found",
+    queueNotFound: "Queue not found"
   },
   releaseNotesController: {
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     releaseNotesTime: (e: any): string =>
-      "Error Parsing release notes time: " + e,
+      "Error Parsing release notes time: " + e
   },
   insightsController: {
     insightUnathorized: "User is not authorized to view this insight",
     insightNameNotFound: "The insight requested was not found",
-    insightsDisabled: "Insights are currently unavailable, sorry :(",
+    insightsDisabled: "Insights are currently unavailable, sorry :("
   },
   roleGuard: {
     notLoggedIn: "Must be logged in",
     noCourseIdFound: "No courseid found",
     notInCourse: "Not In This Course",
     mustBeRoleToJoinCourse: (roles: string[]): string =>
-      `You must have one of roles [${roles.join(", ")}] to access this course`,
+      `You must have one of roles [${roles.join(", ")}] to access this course`
   },
   profileController: {
     accountNotAvailable: "The user account is undefined",
     userResponseNotFound: "The user response was not found",
     noDiskSpace:
-      "There is no disk space left to store an image. Please immediately contact your course staff and let them know. They will contact the Khoury Office Hours team as soon as possible.",
+      "There is no disk space left to store an image. Please immediately contact your course staff and let them know. They will contact the Khoury Office Hours team as soon as possible."
   },
   alertController: {
     duplicateAlert: "This alert has already been sent",
     notActiveAlert: "This is not an alert that's open for the current user",
-    incorrectPayload: "The payload provided was not of the correct type",
+    incorrectPayload: "The payload provided was not of the correct type"
   },
   sseService: {
     getSubClient: "Unable to get the redis subscriber client",
@@ -910,6 +906,6 @@ export const ERROR_MESSAGES = {
     roomMembers: "Unable to get room members",
     serialize: "Unable to serialize payload",
     publish: "Publisher client is unable to publish",
-    clientIdNotFound: "Client ID not found during subscribing to client",
-  },
+    clientIdNotFound: "Client ID not found during subscribing to client"
+  }
 };
--- packages/server/src/course/course.controller.ts
+++ packages/server/src/course/course.controller.ts
@@ -27,10 +27,9 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import async from 'async';
-import moment = require('moment');
 import { EventModel, EventType } from 'profile/event-model.entity';
 import { UserCourseModel } from 'profile/user-course.entity';
-import { Connection, MoreThanOrEqual } from 'typeorm';
+import { Connection } from 'typeorm';
 import { Roles } from '../decorators/roles.decorator';
 import { User, UserId } from '../decorators/user.decorator';
 import { CourseRolesGuard } from '../guards/course-roles.guard';
@@ -38,7 +37,6 @@ import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { UserModel } from '../profile/user.entity';
 import { QueueModel } from '../queue/queue.entity';
 import { CourseModel } from './course.entity';
-import { OfficeHourModel } from './office-hour.entity';
 import { QueueCleanService } from '../queue/queue-clean/queue-clean.service';
 import { QueueSSEService } from '../queue/queue-sse.service';
 import { CourseService } from './course.service';
@@ -114,17 +112,17 @@ export class CourseController {
     if (userCourseModel.role === Role.PROFESSOR) {
       course.queues = await async.filter(
         course.queues,
-        async (q) => !q.isDisabled,
+        async q => !q.isDisabled,
       );
     } else if (userCourseModel.role === Role.TA) {
       course.queues = await async.filter(
         course.queues,
-        async (q) => !q.isDisabled && !q.isProfessorQueue,
+        async q => !q.isDisabled && !q.isProfessorQueue,
       );
     } else if (userCourseModel.role === Role.STUDENT) {
       course.queues = await async.filter(
         course.queues,
-        async (q) => !q.isDisabled && (await q.checkIsOpen()),
+        async q => !q.isDisabled && (await q.checkIsOpen()),
       );
     }
 
@@ -134,7 +132,7 @@ export class CourseController {
     }
 
     try {
-      await async.each(course.queues, async (q) => {
+      await async.each(course.queues, async q => {
         await q.addQueueSize();
       });
     } catch (err) {
@@ -171,7 +169,7 @@ export class CourseController {
 
     if (
       queues &&
-      queues.some((q) => q.staffList.some((staff) => staff.id === user.id))
+      queues.some(q => q.staffList.some(staff => staff.id === user.id))
     ) {
       throw new UnauthorizedException(
         ERROR_MESSAGES.courseController.checkIn.cannotCheckIntoMultipleQueues,
@@ -362,9 +360,9 @@ export class CourseController {
     }
 
     // Do nothing if user not already in stafflist
-    if (!queue.staffList.find((e) => e.id === user.id)) return;
+    if (!queue.staffList.find(e => e.id === user.id)) return;
 
-    queue.staffList = queue.staffList.filter((e) => e.id !== user.id);
+    queue.staffList = queue.staffList.filter(e => e.id !== user.id);
     if (queue.staffList.length === 0) {
       queue.allowQuestions = false;
     }
@@ -401,31 +399,7 @@ export class CourseController {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-    let canClearQueue = null;
 
-    try {
-      canClearQueue = await this.queueCleanService.shouldCleanQueue(queue);
-    } catch (err) {
-      console.error(err);
-      throw new HttpException(
-        ERROR_MESSAGES.courseController.clearQueueError,
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-
-    let nextOfficeHourTime = null;
-
-    // find out how long until next office hour
-    if (canClearQueue) {
-      const soon = moment().add(15, 'minutes').toDate();
-      const nextOfficeHour = await OfficeHourModel.findOne({
-        where: { startTime: MoreThanOrEqual(soon) },
-        order: {
-          startTime: 'ASC',
-        },
-      });
-      nextOfficeHourTime = nextOfficeHour?.startTime;
-    }
     try {
       await this.queueSSEService.updateQueue(queue.id);
     } catch (err) {
@@ -439,7 +413,7 @@ export class CourseController {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-    return { queueId: queue.id, canClearQueue, nextOfficeHourTime };
+    return { queueId: queue.id };
   }
 
   @Post(':id/update_calendar')
@@ -483,7 +457,7 @@ export class CourseController {
     }
 
     return {
-      data: resp.map((row) => ({
+      data: resp.map(row => ({
         id: row.id,
         role: row.role,
         name: row.user.name,
