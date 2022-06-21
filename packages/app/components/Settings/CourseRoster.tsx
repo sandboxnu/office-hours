e11068580d2f41b947de653260366dc80637347e
Iris <Iris.Lamb2@gmail.com>
2022-06-21 10:42:15 -0400

    Fix typo

--- packages/app/components/Settings/CourseRoster.tsx
+++ packages/app/components/Settings/CourseRoster.tsx
@@ -24,7 +24,7 @@ const CourseRosterComponent = styled.div`
 `;
 
 const TableBackground = styled.div`
-  backgroundcolor: white;
+  background-color: white;
 `;
 
 export default function CourseRoster({
