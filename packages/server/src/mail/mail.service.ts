fc58b5b706e6b747fe7b14f69ecc443463893cb9
kevin wang <kevin@kevins-MacBook-Pro-156.local>
2023-01-04 15:52:21 -0800

    Send email changed

---
+++ packages/server/src/mail/mail.service.ts
@@ -0,0 +1,20 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendUserConfirmation(url: string) {
+    await this.mailerService.sendMail({
+      to: 'kevinwang1262000@gmail.com',
+      // from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to Nice App! Confirm your Email',
+      template: './confirmation', // `.hbs` extension is appended automatically
+      context: {
+        // ✏️ filling curly brackets with content
+        name: url,
+      },
+    });
+  }
+}
