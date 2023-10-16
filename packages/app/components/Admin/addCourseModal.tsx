4d47c50e65bb402852fbcb06a3d24399c4d714c8
JasonR24 <ramosjasonm.work@gmail.com>
2023-10-15 21:00:04 -0700

    Configured prettier and tailwindcss formatter

--- packages/app/components/Admin/addCourseModal.tsx
+++ packages/app/components/Admin/addCourseModal.tsx
@@ -1,32 +1,35 @@
-import { ReactElement } from "react";
-import Modal from "antd/lib/modal/Modal";
-import { Switch, Input, Form, Select, Tooltip, message } from "antd";
-import styled from "styled-components";
-import { default as React, useEffect, useState } from "react";
-import { useSemester } from "../../hooks/useSemester";
-import { API } from "@koh/api-client";
+import { ReactElement } from 'react'
+import Modal from 'antd/lib/modal/Modal'
+import { Switch, Input, Form, Select, Tooltip, message } from 'antd'
+import styled from 'styled-components'
+import { default as React, useEffect, useState } from 'react'
+import { useSemester } from '../../hooks/useSemester'
+import { API } from '@koh/api-client'
 const NotesInput = styled(Input.TextArea)`
   border-radius: 6px;
   border: 1px solid #b8c4ce;
-`;
+`
 
 interface AddCourseProps {
-  visible: boolean;
-  onClose: () => void;
+  visible: boolean
+  onClose: () => void
 }
 export function AddCourseModal({
   visible,
   onClose,
 }: AddCourseProps): ReactElement {
-  const [form] = Form.useForm();
-  const [option, setOptions] = useState<{ value: number; label: string }[]>();
-  const semesters = useSemester();
+  const [form] = Form.useForm()
+  const [option, setOptions] = useState<{ value: number; label: string }[]>()
+  const semesters = useSemester()
   useEffect(() => {
     const temp = semesters?.map((semester) => {
-      return { value: semester.id, label: semester.season + semester.year };
-    });
-    setOptions(temp);
-  }, [semesters]);
+      return {
+        value: semester.id,
+        label: semester.season + semester.year,
+      }
+    })
+    setOptions(temp)
+  }, [semesters])
   async function addCourse(value: any) {
     try {
       const body = {
@@ -36,15 +39,15 @@ export function AddCourseModal({
         semester: value.semester,
         enabled: value.enabled,
         timezone: value.timezone,
-      };
-      console.log(body);
-      await API.site_admin.createCourse(body);
-      message.success("Course added");
+      }
+      console.log(body)
+      await API.site_admin.createCourse(body)
+      message.success('Course added')
     } catch (err) {
-      console.log(err.message);
+      console.log(err.message)
       message.error(
-        "Course not added, check whether course has already been added."
-      );
+        'Course not added, check whether course has already been added.',
+      )
     }
   }
 
@@ -54,22 +57,22 @@ export function AddCourseModal({
       visible={visible}
       onCancel={onClose}
       onOk={async () => {
-        const value = await form.validateFields();
-        console.log(value);
-        addCourse(value);
-        onClose();
+        const value = await form.validateFields()
+        console.log(value)
+        addCourse(value)
+        onClose()
       }}
     >
       <Form form={form}>
         <Form.Item label="Name" name="name" required={true}>
-          <NotesInput placeholder={"COSC 404"} />
+          <NotesInput placeholder={'COSC 404'} />
         </Form.Item>
         <Form.Item label="Section name" name="section" required={true}>
-          <NotesInput placeholder={"001"} />
+          <NotesInput placeholder={'001'} />
         </Form.Item>
         <Tooltip title="Use timezone identifiers">
           <Form.Item label="Timezone" name="timezone" required={true}>
-            <NotesInput placeholder={"America/Vancouver"} />
+            <NotesInput placeholder={'America/Vancouver'} />
           </Form.Item>
         </Tooltip>
         <Form.Item
@@ -84,9 +87,9 @@ export function AddCourseModal({
           <Select options={option}></Select>
         </Form.Item>
         <Form.Item label="zoom link" name="zoomlink">
-          <NotesInput placeholder={""} />
+          <NotesInput placeholder={''} />
         </Form.Item>
       </Form>
     </Modal>
-  );
+  )
 }
--- packages/app/components/Settings/ChatbotQuestions.tsx
+++ packages/app/components/Settings/ChatbotQuestions.tsx
@@ -1,67 +1,67 @@
-import { Checkbox, Form, Input, Pagination, Table, Tooltip } from "antd";
-import { FormInstance } from "antd/es/form";
-import { ColumnType, ColumnsType } from "antd/es/table";
-import React, { ReactElement, useEffect, useState } from "react";
-import { API } from "@koh/api-client";
-import { useDebounce } from "../../hooks/useDebounce";
+import { Checkbox, Form, Input, Pagination, Table, Tooltip } from 'antd'
+import { FormInstance } from 'antd/es/form'
+import { ColumnType, ColumnsType } from 'antd/es/table'
+import React, { ReactElement, useEffect, useState } from 'react'
+import { API } from '@koh/api-client'
+import { useDebounce } from '../../hooks/useDebounce'
 
 export interface ChatQuestion {
-  id: string;
-  question: string;
-  answer: string;
-  user: string;
+  id: string
+  question: string
+  answer: string
+  user: string
   sourceDocuments: {
-    name: string;
-    type: string;
-    parts: string[];
-  }[];
-  suggested: boolean;
+    name: string
+    type: string
+    parts: string[]
+  }[]
+  suggested: boolean
 }
 
 export interface ChatQuestionResponse {
-  chatQuestions: ChatQuestion[];
-  total: number;
+  chatQuestions: ChatQuestion[]
+  total: number
 }
 
 export default function ChatbotQuestions(): ReactElement {
-  const [search, setSearch] = useState("");
-  const debouncedValue = useDebounce<string>(search, 500);
+  const [search, setSearch] = useState('')
+  const debouncedValue = useDebounce<string>(search, 500)
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState(10);
-  const [loading, setLoading] = useState(false);
-  const [totalQuestions, setTotalQuestions] = useState(0);
-  const [chatQuestions, setChatQuestions] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1)
+  const [pageSize, setPageSize] = useState(10)
+  const [loading, setLoading] = useState(false)
+  const [totalQuestions, setTotalQuestions] = useState(0)
+  const [chatQuestions, setChatQuestions] = useState([])
 
   const columns: ColumnsType<ChatQuestion> = [
     {
-      title: "Question ID",
-      dataIndex: "id",
-      key: "id"
+      title: 'Question ID',
+      dataIndex: 'id',
+      key: 'id',
     },
     {
-      title: "Question",
-      dataIndex: "question",
-      key: "question"
+      title: 'Question',
+      dataIndex: 'question',
+      key: 'question',
     },
     {
-      title: "Answer",
-      dataIndex: "answer",
-      key: "answer"
+      title: 'Answer',
+      dataIndex: 'answer',
+      key: 'answer',
     },
     {
-      title: "Asked By",
-      dataIndex: "user",
-      key: "user"
+      title: 'Asked By',
+      dataIndex: 'user',
+      key: 'user',
     },
     {
-      title: "Source Documents",
-      dataIndex: "sourceDocuments",
-      key: "sourceDocuments",
+      title: 'Source Documents',
+      dataIndex: 'sourceDocuments',
+      key: 'sourceDocuments',
       render: (_, { sourceDocuments }) => (
         <>
           {sourceDocuments && sourceDocuments.length > 0 ? (
-            sourceDocuments.map(sourceDocument => (
+            sourceDocuments.map((sourceDocument) => (
               // TODO: Create component and refactor into this one
               <p key={sourceDocument.name}>{sourceDocument.name}</p>
             ))
@@ -69,7 +69,7 @@ export default function ChatbotQuestions(): ReactElement {
             <p>No Source Documents</p>
           )}
         </>
-      )
+      ),
     },
     {
       title: () => (
@@ -83,97 +83,97 @@ export default function ChatbotQuestions(): ReactElement {
           </Tooltip>
         </>
       ),
-      dataIndex: "suggested",
-      key: "suggested",
+      dataIndex: 'suggested',
+      key: 'suggested',
       render: (text, record, index) => {
         return (
           <Checkbox
             disabled={loading}
             checked={record.suggested}
-            onChange={e => {
-              toggleSuggested(e.target.checked, index, record.id);
+            onChange={(e) => {
+              toggleSuggested(e.target.checked, index, record.id)
             }}
           />
-        );
-      }
-    }
-  ];
+        )
+      },
+    },
+  ]
 
   useEffect(() => {
-    getQuestions();
-  }, [currentPage, pageSize, debouncedValue]);
+    getQuestions()
+  }, [currentPage, pageSize, debouncedValue])
 
   const toggleSuggested = async (newValue, index, questionId) => {
     // TODO: Loading & contextual disabling
-    setLoading(true);
+    setLoading(true)
     try {
       await API.chatbot.editQuestion({
         data: {
-          suggested: newValue
+          suggested: newValue,
         },
-        questionId
-      });
+        questionId,
+      })
 
-      setChatQuestions(prev => {
-        const newChatQuestions = [...prev];
+      setChatQuestions((prev) => {
+        const newChatQuestions = [...prev]
         newChatQuestions[index] = {
           ...newChatQuestions[index],
-          suggested: newValue
-        };
-        return newChatQuestions;
-      });
+          suggested: newValue,
+        }
+        return newChatQuestions
+      })
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
-    setLoading(false);
-  };
+    setLoading(false)
+  }
 
   const getQuestions = async () => {
-    setLoading(true);
+    setLoading(true)
     try {
       const data: ChatQuestionResponse = await API.chatbot.getQuestions(
         search,
         pageSize,
-        currentPage
-      );
+        currentPage,
+      )
 
-      console.log(data);
-      setChatQuestions(data.chatQuestions);
-      setTotalQuestions(data.total);
+      console.log(data)
+      setChatQuestions(data.chatQuestions)
+      setTotalQuestions(data.total)
     } catch (e) {
-      setChatQuestions([]);
+      setChatQuestions([])
     }
-    setLoading(false);
-  };
+    setLoading(false)
+  }
 
   return (
-    <div className="max-w-[800px] m-auto">
+    <div className="m-auto max-w-[800px]">
       <Input
-        placeholder={"Search question..."}
+        placeholder={'Search question...'}
         // prefix={<SearchOutlined />}
         value={search}
-        onChange={e => {
-          e.preventDefault();
-          setSearch(e.target.value);
+        onChange={(e) => {
+          e.preventDefault()
+          setSearch(e.target.value)
         }}
         onPressEnter={getQuestions}
       />
       <Table
         columns={columns}
         dataSource={chatQuestions}
-        style={{ maxWidth: "800px" }}
+        style={{ maxWidth: '800px' }}
         pagination={false}
       />
       <Pagination
-        style={{ float: "right" }}
+        style={{ float: 'right' }}
         current={currentPage}
         pageSize={pageSize}
         total={totalQuestions}
-        onChange={page => setCurrentPage(page)}
+        onChange={(page) => setCurrentPage(page)}
         pageSizeOptions={[10, 20, 30, 50]}
         showSizeChanger
         onShowSizeChange={(current, pageSize) => setPageSize(pageSize)}
       />
     </div>
-  );
+  )
 }
