a71e733453c1e16ba56631ee5b41a3abdd02fbf3
Adam Fipke <adamfipke@gmail.com>
2023-12-18 14:59:04 -0800

    made hotness bar red + a green-red style

--- packages/app/components/Queue/Student/HotnessBar.tsx
+++ packages/app/components/Queue/Student/HotnessBar.tsx
@@ -1,21 +1,25 @@
 import { ReactElement } from 'react'
 import styled from 'styled-components'
 
+// couldn't figure out how to make the height of the bar dynamic
 const BAR_WIDTH_PX = 10
 const BAR_HEIGHT_PX = 50
 
 /**
  * Renders a vertical bar that shows the hotness of a question.
  * @param hotness a number between 0 (least hot) to 100 (most hot)
+ * @param hasGreen whether or not to have the variation where the bar goes from green to red (as one colour) vs white to red (as a gradient)
  */
 export default function HotnessBar({
   hotness,
+  hasGreen,
 }: {
   hotness: number
+  hasGreen: boolean
 }): ReactElement {
   return (
     <EmptyBar>
-      <Fill hotness={hotness} />
+      <Fill hotness={hotness} hasGreen={hasGreen} />
     </EmptyBar>
   )
 }
@@ -32,12 +36,14 @@ const EmptyBar = styled.div`
 `
 
 // fill of bar, colour and length is determined by hotness
-const Fill = styled.div<{ hotness: number }>`
+const Fill = styled.div<{ hotness: number; hasGreen?: boolean }>`
   width: 100%;
   height: ${({ hotness }) => hotness}%;
-  background-image: ${({ hotness }) =>
-    `linear-gradient(rgb(${255 - hotness * 2.55}, ${
-      255 - hotness * 2.55
-    }, 255), white)`};
+  ${({ hotness, hasGreen = true }) =>
+    hasGreen
+      ? `background-color: rgb(${hotness * 2.55}, ${255 - hotness * 2.55}, 0)`
+      : `background-image: linear-gradient(rgb(255, ${255 - hotness * 2.55}, ${
+          255 - hotness * 2.55
+        }), white)}`};
   border-radius: 5px;
 `
