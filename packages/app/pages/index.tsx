f25e99f34642e7e852fd75d500ee402a43ea4577
Adam Fipke <adamfipke@gmail.com>
2023-12-22 20:13:50 -0800

    simple landing page

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,29 +1,59 @@
-import { User } from '@koh/common'
 import Router from 'next/router'
 import { ReactElement } from 'react'
-import { useHomePageRedirect } from '../hooks/useHomePageRedirect'
-import { useProfile } from '../hooks/useProfile'
-import { Spin } from 'antd'
+import { Button, Image, List } from 'antd'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  width: 500px;
+  padding-top: 100px;
+`
 
 export default function Home(): ReactElement {
-  const profile: User = useProfile()
-  const didRedirect = useHomePageRedirect()
-  if (profile && !didRedirect) {
-    Router.push('/courses')
-  } else {
-    return (
-      <div
-        style={{
-          position: 'absolute',
-          left: '50%',
-          top: '50%',
-          transform: 'translate(-50%, -50%)',
-        }}
-      >
-        <Spin tip="Loading..." size="large" />
-      </div>
-    )
+  const login = () => {
+    Router.push('/login')
   }
 
-  return <div />
+  return (
+    <Container>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <img
+          src="/ubc_logo.png"
+          alt="UBC logo"
+          style={{ width: '3em', margin: '0 1em 0 0' }}
+        />
+        <h1 style={{ display: 'block', alignItems: 'center' }}>
+          Welcome to HelpMe
+        </h1>
+      </div>
+
+      <p style={{ margin: '15px' }}>
+        HelpMe Course System
+        <ul style={{ listStyleType: 'circle', textAlign: 'left' }}>
+          <li style={{ margin: '10px 0' }}>
+            Supports in-person and virtual office hours with instructors and
+            teaching assistants
+          </li>
+          <li style={{ margin: '10px 0' }}>
+            Chatbot for real-time answers about course content and course
+            questions
+          </li>
+          <li style={{ margin: '10px 0' }}>
+            For more information, contact Ramon Lawrence, ramon.lawrence@ubc.ca.
+          </li>
+        </ul>
+      </p>
+      <Button type="primary" onClick={login}>
+        Login
+      </Button>
+    </Container>
+  )
 }
