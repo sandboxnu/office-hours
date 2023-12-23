2d514728610c7c7fa06f3297cf41d71761599fff
Adam Fipke <adamfipke@gmail.com>
2023-12-22 19:33:42 -0800

    simple login redirect

--- packages/app/pages/login.tsx
+++ packages/app/pages/login.tsx
@@ -1,9 +1,13 @@
 import Router from 'next/router'
-import { ReactElement, useState } from 'react'
+import { ReactElement, useState, useEffect } from 'react'
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
 import { message, Button, Form, Input, Alert } from 'antd'
 import styled from 'styled-components'
 import Head from 'next/head'
+import { User } from '@koh/common'
+import { useHomePageRedirect } from '../hooks/useHomePageRedirect'
+import { useProfile } from '../hooks/useProfile'
+import { Spin } from 'antd'
 
 const Container = styled.div`
   margin-left: auto;
@@ -75,72 +79,96 @@ export default function Login(): ReactElement {
     setUname(e.target.value)
   }
 
-  return (
-    <>
-      <Head>
-        <title>Login | HelpMe</title>
-      </Head>
-      <Container>
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{ remember: true }}
-          onFinish={login}
-        >
-          <h1>HelpMe</h1>
-          {!accountActiveResponse && (
-            <Alert
-              message="System Notice"
-              description="Your account has been deactivated. Please contact your organization admin for more information."
-              type="error"
-              style={{ marginBottom: 20, textAlign: 'left' }}
-            />
-          )}
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your Username!' }]}
-            style={{ marginTop: 20 }}
+  // if the user is logged in, redirect them to the courses page
+  const profile: User = useProfile()
+  const didRedirect = useHomePageRedirect()
+  if (profile && !didRedirect) {
+    Router.push('/courses')
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <Spin tip="Loading..." size="large" />
+      </div>
+    )
+  } else {
+    // user not logged in
+    return (
+      <>
+        <Head>
+          <title>Login | HelpMe</title>
+        </Head>
+        <Container>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={login}
           >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              onChange={onUserNameChange}
-              placeholder="Username"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              onChange={onPassChange}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <a
-              style={{ float: 'right', marginTop: '-10px' }}
-              href="/forgetpassword/forget"
+            <h1>HelpMe</h1>
+            {!accountActiveResponse && (
+              <Alert
+                message="System Notice"
+                description="Your account has been deactivated. Please contact your organization admin for more information."
+                type="error"
+                style={{ marginBottom: 20, textAlign: 'left' }}
+              />
+            )}
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: 'Please input your Username!' },
+              ]}
+              style={{ marginTop: 20 }}
             >
-              Forgot password
-            </a>
-          </Form.Item>
-
-          <Form.Item style={{ marginTop: '-15px' }}>
-            <Button
-              style={{ width: '100%', marginTop: '-15px' }}
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                onChange={onUserNameChange}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your Password!' },
+              ]}
             >
-              Log in
-            </Button>
-            Or <a href="/signup/signup">register now!</a>
-          </Form.Item>
-        </Form>
-      </Container>
-    </>
-  )
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                onChange={onPassChange}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <a
+                style={{ float: 'right', marginTop: '-10px' }}
+                href="/forgetpassword/forget"
+              >
+                Forgot password
+              </a>
+            </Form.Item>
+
+            <Form.Item style={{ marginTop: '-15px' }}>
+              <Button
+                style={{ width: '100%', marginTop: '-15px' }}
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+              Or <a href="/signup/signup">register now!</a>
+            </Form.Item>
+          </Form>
+        </Container>
+      </>
+    )
+  }
 }
