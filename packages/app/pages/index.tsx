de3af835bd2a18c6702380d178cab152e3a8ceab
Adam Fipke <adamfipke@gmail.com>
2023-12-26 13:29:02 -0800

    tiny cleanup

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,25 +1,11 @@
 import { ReactElement } from 'react'
-import { Button, Image, List } from 'antd'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  text-align: center;
-  width: 500px;
-  padding-top: 100px;
-`
+import { Button } from 'antd'
 
 export default function Home(): ReactElement {
   return (
-    <Container>
+    <div className="ml-auto mr-auto w-[500px] pt-10 text-center">
       <div className="flex flex-col items-center justify-center">
-        <img
-          src="/ubc_logo.png"
-          // src="/favicon.ico"
-          alt="UBC logo"
-          className="mb-4 mr-4 w-12"
-        />
+        <img src="/ubc_logo.png" alt="UBC logo" className="mb-4 mr-4 w-12" />
         <h1>Welcome to HelpMe</h1>
       </div>
 
@@ -43,6 +29,6 @@ export default function Home(): ReactElement {
       <Button type="primary" className="rounded-lg" href="/login">
         Login &gt;
       </Button>
-    </Container>
+    </div>
   )
 }
